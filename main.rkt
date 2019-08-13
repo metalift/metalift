7019a94e3542af6c6dbd925ff988e6fabc81c1f3
Alvin Cheung <akcheung@cs.washington.edu>
2019-08-12 23:34:41 -0700

    clean up

--- main.rkt
+++ main.rkt
@@ -1,32 +1,34 @@
 #lang racket
 
 ; entry point of metalift
-; 
-(define-syntax (@#%module-begin stx) 
-  (syntax-case stx ()
 
-    ; right now we just return the input stx as-is. later on we might want to optimize the input code    
-    [(a forms ...)     
-     (let* ([core (local-expand #'(#%plain-module-begin forms ...) 'module-begin (list #'module*))]
-            ;[vc (compute-vc core)]
-            ;([core (local-expand #'(#%plain-module-begin forms ...) 'expression (list #'for))] ;#'module* ))]
-            ;([core #`(#%module-begin #,@(local-expand #'(forms ...) 'expression (list #'for)))] ;#'module* ))]
-            ;[vars (find-mutated-vars core)]
-            )
-           ; [transformed (box-mutated-vars core vars)])
-             
-       ;core
-              
-       #'(#%module-begin
-          forms ...
-          )
-     )]
-    ))
+; we don'g need to redefine module-begin as we current don't need whole program info
+;(define-syntax (@#%module-begin stx) 
+;  (syntax-case stx ()
+;
+;    ; right now we just return the input stx as-is. later on we might want to optimize the input code    
+;    [(a forms ...)     
+;     (let* ([core (local-expand #'(#%plain-module-begin forms ...) 'module-begin (list #'module*))]
+;            ;[vc (compute-vc core)]
+;            ;([core (local-expand #'(#%plain-module-begin forms ...) 'expression (list #'for))] ;#'module* ))]
+;            ;([core #`(#%module-begin #,@(local-expand #'(forms ...) 'expression (list #'for)))] ;#'module* ))]
+;            ;[vars (find-mutated-vars core)]
+;            )
+;           ; [transformed (box-mutated-vars core vars)])
+;             
+;       ;core
+;              
+;       #'(#%module-begin
+;          forms ...
+;          )
+;     )]
+;    ))
 
 (require "lang/base.rkt")
 
 (provide
- (rename-out [@#%module-begin #%module-begin])
+ ;(rename-out [@#%module-begin #%module-begin])
+ quote #%module-begin
  quote #%datum
  quote #%app
  quote #%top-interaction
