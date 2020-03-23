77ec5c5b75d601c8a83225580e764a9104f011b7
Alvin Cheung <akcheung@cs.washington.edu>
2020-03-23 10:28:05 -0700

    minor cleanup

--- examples/simple_loop/compiler.py
+++ examples/simple_loop/compiler.py
@@ -3,13 +3,14 @@ from typing import Dict
 
 from cfg import CFGBuilder
 from frontend import python
-import ir
+from ir import Block, num, Call, BinaryOp, While, Var, Stmt, If, Return, Lit, Choose
+
 from synth import synthesize
 from vcgen import VCGen, LoopTransformer
 from visitor import PassthruVisitor
 
 # input a list of vars used in the code fragment and return a ML program
-def inv_space_fn1(ast: ir.While, read_vars: Dict[str, ir.Var], write_vars: Dict[str, ir.Var]) -> ir.Stmt:
+def inv_space_fn1(ast: While, read_vars: Dict[str, Var], write_vars: Dict[str, Var]) -> Stmt:
   sum = write_vars["sum"]
   i = write_vars["i"]
   n = read_vars["n"]
@@ -18,12 +19,12 @@ def inv_space_fn1(ast: ir.While, read_vars: Dict[str, ir.Var], write_vars: Dict[
   # if i <= n: i >= 0 and sum = my_sum(i)
   # else: sum = 0
   #
-  return ir.Block(ir.If(ir.BinaryOp(operator.__le__, i, n),
-                        ir.Return(ir.BinaryOp(operator.and_, ir.BinaryOp(operator.ge, i, ir.num(0)),
-                                              ir.BinaryOp(operator.eq, sum, ir.Call("my_sum", i)))),
-                        ir.Return(ir.BinaryOp(operator.eq, sum, ir.num(0)))))
+  return Block(If(BinaryOp(operator.le, i, n),
+                  Return(BinaryOp(operator.and_, BinaryOp(operator.ge, i, num(0)),
+                                                 BinaryOp(operator.eq, sum, Call("my_sum", i)))),
+                  Return(BinaryOp(operator.eq, sum, num(0)))))
 
-def inv_space_fn2(ast: ir.While, read_vars: Dict[str, ir.Var], write_vars: Dict[str, ir.Var]) -> ir.Stmt:
+def inv_space_fn2(ast: While, read_vars: Dict[str, Var], write_vars: Dict[str, Var]) -> Stmt:
   sum = write_vars["sum"]
   i = write_vars["i"]
   n = read_vars["n"]
@@ -32,13 +33,13 @@ def inv_space_fn2(ast: ir.While, read_vars: Dict[str, ir.Var], write_vars: Dict[
   # if i <= n: i >= 0 and sum = my_sum(choose(i, n))
   # else: sum = 0
   #
-  return ir.Block(ir.If(ir.BinaryOp(operator.__le__, i, n),
-                        ir.Return(ir.BinaryOp(operator.and_, ir.BinaryOp(operator.ge, i, ir.num(0)),
-                                              ir.BinaryOp(operator.eq, sum, ir.Call("my_sum", ir.Choose(n, i))))),
-                        ir.Return(ir.BinaryOp(operator.eq, sum, ir.num(0)))))
+  return Block(If(BinaryOp(operator.le, i, n),
+                  Return(BinaryOp(operator.and_, BinaryOp(operator.ge, i, num(0)),
+                                                 BinaryOp(operator.eq, sum, Call("my_sum", Choose(n, i))))),
+                  Return(BinaryOp(operator.eq, sum, num(0)))))
 
 
-def ps_space_fn1(ast: ir.Stmt, read_vars: Dict[str, ir.Var], write_vars: Dict[str, ir.Var]) -> ir.Stmt:
+def ps_space_fn1(ast: Stmt, read_vars: Dict[str, Var], write_vars: Dict[str, Var]) -> Stmt:
   sum = write_vars["sum"]
   rv = write_vars["rv"]
   n = read_vars["n"]
@@ -46,13 +47,11 @@ def ps_space_fn1(ast: ir.Stmt, read_vars: Dict[str, ir.Var], write_vars: Dict[st
   # if 0 <= n: sum = my_sum(n) and rv = sum
   # else: sum = 0 and rv = sum
   #
-  return ir.Block(ir.If(ir.BinaryOp(operator.__le__, ir.num(0), n),
-                        ir.Return(ir.BinaryOp(operator.__and__,
-                                                ir.BinaryOp(operator.eq, sum, ir.Call("my_sum", n)),
-                                                ir.BinaryOp(operator.eq, rv, sum))),
-                        ir.Return(ir.BinaryOp(operator.__and__,
-                                                ir.BinaryOp(operator.eq, sum, ir.num(0)),
-                                                ir.BinaryOp(operator.eq, rv, sum)))))
+  return Block(If(BinaryOp(operator.le, num(0), n),
+                  Return(BinaryOp(operator.and_, BinaryOp(operator.eq, sum, Call("my_sum", n)),
+                                                 BinaryOp(operator.eq, rv, sum))),
+                  Return(BinaryOp(operator.and_, BinaryOp(operator.eq, sum, num(0)),
+                                                 BinaryOp(operator.eq, rv, sum)))))
 
 
 class CodeGenerator(PassthruVisitor):
@@ -63,10 +62,10 @@ class CodeGenerator(PassthruVisitor):
   def visit_Var(self, n):
     return n.name
 
-  def visit_Lit(self, n: ir.Lit):
+  def visit_Lit(self, n: Lit):
     return str(n.val)
 
-  def visit_BinaryOp(self, n: ir.BinaryOp):
+  def visit_BinaryOp(self, n: BinaryOp):
     ops = { operator.add: "+", operator.sub: "-", operator.eq: "=", operator.__le__: "<=", operator.and_: "and"}
     left, right = n.args[0], n.args[1]
     if n.op == operator.and_:
@@ -74,13 +73,13 @@ class CodeGenerator(PassthruVisitor):
     else:
       return "%s %s %s" % (self.visit(left), ops[n.op], self.visit(right))
 
-  def visit_If(self, n: ir.If):
+  def visit_If(self, n: If):
     return "if %s:\n    %s\n  else:\n    %s" % (self.visit(n.cond), self.visit(n.conseq), self.visit(n.alt))
 
-  def visit_Return(self, n: ir.Return):
+  def visit_Return(self, n: Return):
     return self.visit(n.body)
 
-  def visit_Block(self, n: ir.Block):
+  def visit_Block(self, n: Block):
     return "\n".join("  " + self.visit(s) for s in n.stmts)
 
   def visit_FnDecl(self, n):
@@ -91,6 +90,8 @@ def codegen(ps, orig_fn):
   return CodeGenerator(orig_fn).visit(ps)
 
 
+### Main Compiler ###
+
 t = python.translate_file("input.py")
 l = python.translate_file("udo.py")
 
@@ -98,15 +99,13 @@ fn = t.fns["input"]
 
 c = CFGBuilder()
 c.construct_cfg(fn)
-lt = LoopTransformer(c)
-(r, info) = lt.visit(fn)
+(r, info) = LoopTransformer(c).visit(fn)
 #print("after loop transform: %s" % r)
 
-vcgen = VCGen()
-r = vcgen.visit(r)
+r = VCGen().visit(r)
 #print("after vcgen: %s" % r)
 
-r = synthesize(r, l, info, inv_space_fn2, ps_space_fn1)
+r = synthesize(r, l, info, inv_space_fn1, ps_space_fn1)
 #print("after synthesis: %s" % r)
 
 c = codegen(r.fns["input_ps"], fn)
