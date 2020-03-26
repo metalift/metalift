2fd05ec9c531a00be24da64f0c9d16f19c9fedf8
Alvin Cheung <akcheung@cs.washington.edu>
2020-03-25 23:53:26 -0700

    bug fix

--- examples/simple_loop/verify.rkt
+++ examples/simple_loop/verify.rkt
@@ -14,23 +14,41 @@
 
 (define-symbolic my_sum (~> integer? integer?))
 (assert (forall (list end)
-(begin (if (> end 0) (begin (eq? (my_sum end) (+ 1 (my_sum (- end 1))))) (begin (eq? (my_sum end) 0))))))
+
+(begin (if (> end 0)
+           (begin (eq? (my_sum end) (+ 1 (my_sum (- end 1)))))
+           (begin (eq? (my_sum end) 0))))))
+
 
 (define-symbolic vctest_ps (~> integer? integer? integer? integer? boolean?))
 (assert (forall (list n sum i rv)
-(begin
-  (if (<= 0 n)
-      (eq? (vctest_ps n sum i rv) (and (eq? sum (my_sum n)) (eq? rv sum))) (eq? (vctest_ps n sum i rv) (and (eq? sum 0) (eq? rv sum)))))))
+(begin    
+  (eq? (vctest_ps n sum i rv) (if (<= 0 n)
+                                  (and (eq? sum (my_sum n)) (eq? sum rv))
+                                  (and (eq? sum 0) (eq? sum rv)))))))
 
-(define (inv_0 n sum i) 
-(begin (if (<= 0 n) (and (<= i n) (eq? sum (my_sum i))) (eq? sum 0))))
+; this also works
+;(define (vctest_ps n sum i rv)
+;  (if (<= 0 n)
+;      (and (eq? sum (my_sum n) (eq? sum rv))
+;      (and (eq? sum 0) (eq? sum rv)))
 
 
-(assert (forall (list sum_1 n i_0) (not
+(define (inv_0 n sum i)
+  (if (and (<= n i) (<= n 0))
+      (eq? sum 0)
+      (and (>= i 0) (<= i n) (eq? sum (my_sum i))))
+)
+
+
+(assert (not (forall (list sum_1 n i_0)
   (and
-    (inv_0 n 0 0)
-    (or (not (and (inv_0 n sum_1 i_0) (< i_0 n))) (inv_0 n (+ sum_1 1) (+ i_0 1)))
-    (or (not (and (inv_0 n sum_1 i_0) (not (< i_0 n)))) (vctest_ps n sum_1 i_0 sum_1))
+   (inv_0 n 0 0)
+
+   (or (not (and (inv_0 n sum_1 i_0) (< i_0 n))) (inv_0 n (+ sum_1 1) (+ i_0 1)))
+   
+   (or (not (and (inv_0 n sum_1 i_0) (not (< i_0 n)))) (vctest_ps n sum_1 i_0 sum_1))
+
     ))))
 
 (solve (void))
