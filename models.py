cdd944899a00e248199aaa3fa7ff834da6568508
Sahil Bhatia <sahilbhatia.nsit@gmail.com>
2021-10-15 00:28:06 -0700

    fixed models.py

--- models.py
+++ models.py
@@ -1,23 +1,23 @@
 from collections import namedtuple
 
-from ir import Expr, Type, parseTypeRef
+from ir import Expr, Type, parseTypeRef, Int, Bool, Var, Call
 
 ReturnValue = namedtuple("ReturnValue", ["val", "assigns"])
 
 def newlist(regs, *args):
   # return ReturnValue(None, [(args[0], Expr.Pred("list_new", parseTypeRef(args[0].type)))])
-  return ReturnValue(Expr.Var("list_empty", Type("MLList", Type.int())), None)
+  return ReturnValue(Var("list_empty", Type("MLList", Int())), None)
 
 
 def listLength(regs, *args):
-  return ReturnValue(Expr.Pred("list_length", Type.int(), regs[args[0]]), None)
+  return ReturnValue(Call("list_length", Int(), regs[args[0]]), None)
 
 def listGet(regs, *args):
-  return ReturnValue(Expr.Pred("list_get", Type.int(), regs[args[0]], regs[args[1]]), None)
+  return ReturnValue(Call("list_get", Int(), regs[args[0]], regs[args[1]]), None)
 
 def listAppend(regs, *args):
   # return ReturnValue(None, [(args[0], Expr.Pred("list_append", parseTypeRef(args[0].type), regs[args[1]]))])
-  return ReturnValue(Expr.Pred("list_append", parseTypeRef(args[0].type), regs[args[0]], regs[args[1]]), None)
+  return ReturnValue(Call("list_append", parseTypeRef(args[0].type), regs[args[0]], regs[args[1]]), None)
 
 fnModels = {
   # mangled names for non template version of list.h
