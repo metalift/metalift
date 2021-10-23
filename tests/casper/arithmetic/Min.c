120217cfcc558ca84042d5db9eeaf1c284568b56
Justin Wong <wong.justin@berkeley.edu>
2021-10-23 15:40:49 -0700

    translate some more

--- tests/casper/arithmetic/Min.c
+++ tests/casper/arithmetic/Min.c
@@ -5,7 +5,7 @@
 int test(List<int> data) {
 	int min = __INT_MAX__;
 	for(int i=0; i<listLength(data); i++) {
-		int val = listGet(data);
+		int val = listGet(data, i);
 		if(min > val)
 			min = val;
 	}
