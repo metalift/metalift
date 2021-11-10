b6fabdf1bf41e407481a9c32bdcfdbd80167f603
Justin Wong <wong.justin@berkeley.edu>
2021-11-09 20:07:59 -0500

    fix cycling

--- tests/casper/bigλ/CyclingSpeed.c
+++ tests/casper/bigλ/CyclingSpeed.c
@@ -4,14 +4,14 @@
 
        
 List<int> main(List<int> fst_list, List<int> snd_list, List<int> emit_list, List<int> speed_list) {
-    List<int> result;
+    Array<int> result;
 
     for(int i = 0; i < listLength(speed_list) ; i++) {
-      int speed = listGet(speed_list, i);
-      if(!listGet(result,speed) != NONE) {
-        listSet(result,speed,0)];
+      int speed = arrayGet(speed_list, i);
+      if(!arrayGet(result,speed) != NONE) {
+        arraySet(result,speed,0)];
       }
-      listSet (result,speed, listGet(result,speed)+1);
+      arraySet (result,speed, arrayGet(result,speed)+1);
     }
 
     return result;
