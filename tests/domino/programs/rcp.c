75034c83aa27f1279f3e0df29d97251e88bebf8f
Sumer Kohli <sumer.kohli@berkeley.edu>
2022-01-09 11:50:48 -0800

    refactor(tests): Start updating Domino tests for pairs, fast-forward from llvm branch

--- tests/domino/programs/rcp.c
+++ tests/domino/programs/rcp.c
@@ -22,4 +22,7 @@ int func(int size_bytes, int rtt, int _input_traffic_Bytes, int _sum_rtt_Tr, int
         sum_rtt_Tr += pkt.rtt;
         num_pkts_with_rtt += 1;
     }
+
+    int sum = 0;
+    sum = size_bytes + rtt
 }
--- tests/list.h
+++ tests/list.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cstdarg>
 
 template <typename T>
 struct list
@@ -11,27 +12,42 @@ using List = list<T> *;
 
 
 template <class T>
-int listLength (List<T> l) 
+int listLength (List<T> l)
 {
   return l->contents.size();
 }
 
 template <class T>
-List<T> newList() 
+List<T> newList()
 {
   //return (List<T>)malloc(sizeof(struct list));
   return new list<T>();
 }
 
 template <class T>
-T listGet (List<T> l, int i) 
-{ 
+List<T> newListOf(int len, T args...) {
+  List<T> ret = new list<T>();
+  std::va_list args;
+
+  va_start(args, count);
+  for (int i = 0; i < count; ++i)
+  {
+    ret = listAppend(ret, va_arg(args, T));
+  }
+  va_end(args);
+
+  return ret;
+}
+
+template <class T>
+T listGet (List<T> l, int i)
+{
   return l->contents[i];
 }
 
 
 template <class T>
-List<T> listAppend (List<T> in, T e) 
+List<T> listAppend (List<T> in, T e)
 {
   List<T> r = newList<T>();
   for (int i = 0; i < listLength(in); ++i)
@@ -41,7 +57,7 @@ List<T> listAppend (List<T> in, T e)
 }
 
 template <class T>
-List<T> listConcat (List<T> in, List<T> e) 
+List<T> listConcat (List<T> in, List<T> e)
 {
   List<T> r = newList<T>();
   for (int i = 0; i < listLength(in); ++i)
