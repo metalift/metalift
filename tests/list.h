cee50f0ad079c277b31f6e46019f6d0c337525db
Sumer Kohli <sumer.kohli@berkeley.edu>
2022-03-12 19:43:09 -0800

    refactor(lists): add convenience functions

--- tests/list.h
+++ tests/list.h
@@ -1,71 +1,74 @@
-#include <vector>
 #include <cstdarg>
+#include <vector>
 
 template <typename T>
-struct list
-{
-  std::vector<T> contents;
+struct list {
+    std::vector<T> contents;
 };
 
 template <typename T>
 using List = list<T> *;
 
-
 template <class T>
-int listLength (List<T> l)
-{
-  return l->contents.size();
+int listLength(List<T> l) {
+    return l->contents.size();
 }
 
 template <class T>
-List<T> newList()
-{
-  //return (List<T>)malloc(sizeof(struct list));
-  return new list<T>();
+List<T> newList() {
+    // return (List<T>)malloc(sizeof(struct list));
+    return new list<T>();
 }
 
 template <class T>
-List<T> newListOf(int len, T args...) {
-  List<T> ret = new list<T>();
-  int count;
-  va_start(args, count);
-  for (int i = 0; i < count; ++i)
-  {
-    ret = listAppend(ret, va_arg(args, T));
-  }
-  va_end(args);
-
-  return ret;
+T listGet(List<T> l, int i) {
+    return l->contents[i];
 }
 
 template <class T>
-T listGet (List<T> l, int i)
-{
-  return l->contents[i];
-}
-
-
-template <class T>
-List<T> listAppend (List<T> in, T e)
-{
-  List<T> r = newList<T>();
-  for (int i = 0; i < listLength(in); ++i)
-    r->contents.push_back(listGet(in, i));
-  r->contents.push_back(e);
-  return r;
+List<T> listAppend(List<T> in, T e) {
+    List<T> r = newList<T>();
+    for (int i = 0; i < listLength(in); ++i)
+        r->contents.push_back(listGet(in, i));
+    r->contents.push_back(e);
+    return r;
 }
 
 template <class T>
-List<T> listConcat (List<T> in, List<T> e)
-{
-  List<T> r = newList<T>();
-  for (int i = 0; i < listLength(in); ++i)
-    r->contents.push_back(listGet(in, i));
-  for (int i = 0; i < listLength(e); ++i)
-    r->contents.push_back(listGet(e, i));
-  return r;
+List<T> listConcat(List<T> in, List<T> e) {
+    List<T> r = newList<T>();
+    for (int i = 0; i < listLength(in); ++i)
+        r->contents.push_back(listGet(in, i));
+    for (int i = 0; i < listLength(e); ++i)
+        r->contents.push_back(listGet(e, i));
+    return r;
 }
 
+template <typename T>
+List<T> newListOfReverse(T first) {
+    List<T> ret = new list<T>();
+    ret = listAppend(ret, first);
+    return ret;
+}
 
+/** Create list of provided arguments (variable number) in reverse order */
+template <typename T, typename... Args>
+List<T> newListOfReverse(T first, Args... args) {
+    return listAppend(newListOfReverse(args...), first);
+}
 
+template <typename T>
+List<T> newListOf(T first) {
+    return listAppend(newList<T>(), first);
+}
 
+template <typename T>
+List<T> newListOf(T first, T second) {
+    return listAppend(listAppend(newList<T>(), first), second);
+}
+
+template <typename T>
+List<T> newListOf(T first, T second, T third) {
+    return listAppend(listAppend(listAppend(newList<T>(), first), second),
+                      third);
+}
