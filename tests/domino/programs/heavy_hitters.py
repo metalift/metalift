e4460b4d2df4b7bea9f7b65398fb46fd017457b4
Sumer Kohli <sumer.kohli@berkeley.edu>
2022-03-15 11:48:34 -0700

    feat(domino/benchmarks): add in-progress Heavy Hitters benchmark (4/6 stages working)

---
+++ tests/domino/programs/heavy_hitters.py
@@ -0,0 +1,84 @@
+import os
+
+from ir import *
+
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from domino import DominoLang
+
+domino = DominoLang()
+
+"""
+"mul_acc",
+"nested_ifs",
+"sub",
+"add_2_state_vars",
+"add_3_state_vars",
+"if_else_raw",
+"stateless_arith",
+"""
+
+
+if __name__ == "__main__":
+    components = [
+        (
+            "stage0",
+            {
+                "depth": 2,
+                "atoms": [
+                    "add_3_state_vars",
+                    # "add_2_state_vars",
+                    # "add_state_var",
+                    # "stateless_arith",
+                ],
+            },
+            {
+                "uninterpFuncs": [
+                    ("uninterpHash2a", 2),
+                    ("uninterpHash2b", 2),
+                    ("uninterpHash2c", 2),
+                ],
+                "listBound": 3,
+            },
+        ),
+        (
+            "stage1",
+            {
+                "depth": 2,
+                "atoms": [
+                    "add_3_state_vars",
+                    # "add_2_state_vars",
+                    # "add_state_var",
+                    # "stateless_arith",
+                ],
+            },
+            {
+                "uninterpFuncs": [
+                    ("uninterpReadSketch1At", 1),
+                    ("uninterpReadSketch2At", 1),
+                    ("uninterpReadSketch3At", 1),
+                ],
+                "listBound": 3,
+            },
+        ),
+        # (
+        #     "stage2p1",
+        #     {"depth": 4, "atoms": ["if_else_raw", "add_state_var"]},
+        # ),
+        (
+            "stage2p2",
+            {"depth": 3, "atoms": ["if_else_raw", "add_state_var"]},
+        ),
+        (
+            "stage2p3",
+            {"depth": 3, "atoms": ["if_else_raw", "add_state_var"]},
+        ),
+        (
+            "stage3",
+            {"depth": 2, "atoms": ["add_3_state_vars", "stateless_arith"]},
+        ),
+    ]
+
+    domino.multiple_component_driver(components)
