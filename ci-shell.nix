d36cd0d6b63797daf5101c182d09a55740fdeaf5
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-04-26 17:55:54 -0700

    Update Nix channels and use upstreamed cvc5 (#63)

--- ci-shell.nix
+++ ci-shell.nix
@@ -1,21 +1,17 @@
 let
   # updated 3/15/22
-  pkgs = import (fetchTarball https://github.com/nixos/nixpkgs/archive/0f85665118d850aae5164d385d24783d0b16cf1b.tar.gz) { };
-  unstable = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/b0e141e3fe13ec21f50429773d2e3890e02a80da.tar.gz) { };
+  pkgs = import (fetchTarball https://github.com/nixos/nixpkgs/archive/3a9e0f239d80fa134e8fcbdee4dfc793902da37e.tar.gz) { };
+  unstable = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/6a323903ad07de6680169bb0423c5cea9db41d82.tar.gz) { };
 in
 with pkgs;
 
-let
-  cvc5 = callPackage ./cvc5.nix { unstable = unstable; };
-in
 mkShell {
-  venvDir = "./.venv";
   buildInputs = [
     (python38.withPackages (p: with p; [
       pyparsing
     ]))
 
-    cvc5
+    unstable.cvc5
     cmake
     llvm_11
     clang_11
