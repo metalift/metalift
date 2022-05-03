a3dc213314ed203336f401b32b6b75c3ca8faa2f
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-05-02 21:32:09 -0700

    Improve landing page title

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -38,8 +38,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
