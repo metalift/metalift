97026077e447e1b6225ef36d703a23b77e76b227
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-05-24 22:56:58 -0700

    Fix broken Nix cache

--- ci-shell.nix
+++ ci-shell.nix
@@ -1,7 +1,7 @@
 let
   # updated 3/15/22
-  pkgs = import (fetchTarball https://github.com/nixos/nixpkgs/archive/3a9e0f239d80fa134e8fcbdee4dfc793902da37e.tar.gz) { };
-  unstable = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/6a323903ad07de6680169bb0423c5cea9db41d82.tar.gz) { };
+  pkgs = import (fetchTarball https://github.com/nixos/nixpkgs/archive/cbd40c72b2603ab54e7208f99f9b35fc158bc009.tar.gz) { };
+  unstable = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/dfd82985c273aac6eced03625f454b334daae2e8.tar.gz) { };
 in
 with pkgs;
 
