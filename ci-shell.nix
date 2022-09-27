d966c78828bd15aa7cd3c7cf2c20c24fbe88407a
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-09-27 16:44:10 -0700

    Pin llvmlite to specific revision

--- ci-shell.nix
+++ ci-shell.nix
@@ -1,7 +1,7 @@
 let
-  # updated 7/11/22
-  pkgs = import (fetchTarball https://github.com/nixos/nixpkgs/archive/ccf8bdf72624521358be6bb7d9b524c4cbcf7aff.tar.gz) { };
-  unstable = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/b39924fc7764c08ae3b51beef9a3518c414cdb7d.tar.gz) { };
+  # updated 9/27/22
+  pkgs = import (fetchTarball https://github.com/nixos/nixpkgs/archive/82379884b2e9cf1ba65f5b14bbcb9d1438abb745.tar.gz) { };
+  unstable = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/4dd035239719fc7992a4214934ecaf2a943d5ec0.tar.gz) { };
   poetry2nixLatest = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/2a2193eb0677a8801c3b414f67bacf499bd0b6fc.tar.gz) { };
 in
 with pkgs;
