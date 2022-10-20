1a324eea3e34433d13d3e2ae4391b13e5ecf7422
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-10-20 00:39:12 -0700

    Migrate to Nix flakes to improve reproducibility

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "katara";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.poetry2nix-flake = {
+    url = "github:nix-community/poetry2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  inputs.llvmlite-patched = {
+    flake = false;
+    url = "https://github.com/metalift/llvmlite.git";
+    type = "git";
+    ref = "patched-metalift";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix-flake, llvmlite-patched }: (flake-utils.lib.eachDefaultSystem (system:
+    with import nixpkgs {
+      inherit system;
+      overlays = [ poetry2nix-flake.overlay ];
+    };
+
+    {
+      devShell = mkShell {
+        buildInputs = [
+          (poetry2nix.mkPoetryEnv {
+            python = python38;
+            projectDir = ./.;
+            preferWheels = true;
+
+            overrides = poetry2nix.overrides.withDefaults (_: poetrySuper: {
+              llvmlite = poetrySuper.llvmlite.overridePythonAttrs(super: {
+                src = llvmlite-patched;
+              });
+
+              autoflake = poetrySuper.autoflake.overrideAttrs(_: super: {
+                nativeBuildInputs = super.nativeBuildInputs ++ [ poetrySuper.hatchling ];
+              });
+            });
+          })
+
+          cvc5
+          cmake
+          llvm_11
+          clang_11
+        ];
+      };
+    }
+  ));
+}
