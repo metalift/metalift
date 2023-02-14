95fbf2efa5089b8983b101e43d6e243bece61ff2
Yuto Nishida <yuto@berkeley.edu>
2023-02-14 13:16:00 -0800

    Use numpy api for codegen

--- tests/mat2.py
+++ tests/mat2.py
@@ -79,30 +79,37 @@ lang = targetLang()
 candidates = synthesize(basename, lang, vars, invAndPs, preds, vc, loopAndPsInfo, cvcPath)
 
 def codeGen(summary: FnDecl):
-  expr = summary.body() 
-  def eval(expr):
-    if isinstance(expr, Eq):
-      return f"ans = {eval(expr.e2())}"
-    elif isinstance(expr, Add):
-      return f"{eval(expr.args[0])} + {eval(expr.args[1])}"
-    elif isinstance(expr, Call):
-      eval_args = []
-      for a in expr.arguments():
-        eval_args.append(eval(a))
-      name = expr.name()
-      if name == MAT_MUL:
-        name = "np.matmul"
-      elif name == L1_NORM:
-        name = "2 * np.sum"
-      return f"{name}({', '.join(eval_args)})"
-    elif isinstance(expr, Lit):
-      return str(expr.val())
-    elif isinstance(expr, Tuple):
-      eval_args = map(lambda expr: eval(expr), expr.args)
-      return f"[{', '.join(eval_args)}]"
-    else:
-      return str(expr)
-  return eval(expr)
+    expr = summary.body() 
+    def eval(expr):
+        if isinstance(expr, Eq):
+            return f"ans = {eval(expr.e2())}"
+        elif isinstance(expr, Add):
+            return f"{eval(expr.args[0])} + {eval(expr.args[1])}"
+        elif isinstance(expr, Call):
+            eval_args = []
+            for a in expr.arguments():
+                eval_args.append(eval(a))
+            name = expr.name()
+            if name == MAT_MUL:
+                # Pack args
+                name = "np.matmul"
+                args = expr.arguments()
+                assert(len(args) == 3)
+                M = f"np.array([{eval(args[0])}, {eval(args[1])}]).T"
+                x = eval(args[2])
+                return f"{name}({M}, {x})"
+            elif name == L1_NORM:
+                name = "np.linalg.norm"
+                return f"{name}({', '.join(eval_args)}, ord=1)"
+            return f"{name}({', '.join(eval_args)})"
+        elif isinstance(expr, Lit):
+            return str(expr.val())
+        elif isinstance(expr, Tuple):
+            eval_args = map(lambda expr: eval(expr), expr.args)
+            return f"[{', '.join(eval_args)}]"
+        else:
+            return str(expr)
+    return eval(expr)
 
 summary = codeGen(candidates[0])
 
