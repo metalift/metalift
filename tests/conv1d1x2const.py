6cbb968a5505028414a462ed517cce31d2229322
Yuto Nishida <yuto@berkeley.edu>
2023-03-01 15:38:39 -0800

    wip conv1d1x2const

---
+++ tests/conv1d1x2const.py
@@ -0,0 +1,171 @@
+import shutil
+
+from metalift.ir import *
+from metalift.analysis import CodeInfo, analyze
+
+from metalift.synthesize_auto import synthesize
+
+MUL1D = "elementwise_mul"
+SAME_LEN = "same_length"
+CONV1D1X2 = "conv1d1x2"
+DOTPROD2D = "dotprod_2d"
+
+def ml_list_get(lst, i):
+    return Call("list_get", Int(), lst, i)
+
+def ml_list_head(lst):
+    return ml_list_get(lst, IntLit(0))
+
+def ml_list_tail(lst, i):
+    return Call("list_tail", ListT(Int()), lst, i)
+
+def ml_list_prepend(e, lst):
+    return Call("list_prepend", ListT(Int()), e, lst)
+
+def ml_list_length(lst):
+    return Call("list_length", Int(), lst)
+
+def ml_list_empty():
+    return Call("list_empty", ListT(Int()))
+
+def ml_list_take(lst, i):
+    return Call("list_take", ListT(Int()), lst, i)
+
+def ml_min(a, b):
+    return Ite(Lt(a, b), a, b)
+
+def ml_dotprod2d(x, y):
+    return Call(DOTPROD2D, Int(), x, y)
+
+def ml_conv1d1x2(vec, kernel):
+    return Call(CONV1D1X2, ListT(Int()), vec, kernel)
+
+def grammar(ci: CodeInfo):
+    name = ci.name
+
+    if name.startswith("inv"):
+        # mV[0] is list, mV[1] is int
+        print("INV VARS MV HERE")
+        print(*ci.modifiedVars)
+        print("INV VARS RV HERE")
+        print(*ci.readVars)
+        an_input = Choose(*ci.readVars)
+        #an_output = Choose(*ci.modifiedVars)
+        #an_output_i32 = Choose(ci.modifiedVars[0], ci.modifiedVars[1], ci.modifiedVars[2], ci.modifiedVars[4])
+        #an_output_list = ci.modifiedVars[3]
+        an_output_i32 = ci.modifiedVars[1]
+        an_output_list = ci.modifiedVars[0]
+
+        initial = Choose(Ge(an_output_i32, IntLit(0)),
+                         Gt(an_output_i32, IntLit(0)),
+                         Le(an_output_i32, IntLit(0)),
+                         Lt(an_output_i32, IntLit(0)),
+                         Eq(an_output_i32, IntLit(0)),
+                         Ge(an_output_i32, IntLit(1)),
+                         Gt(an_output_i32, IntLit(1)),
+                         Le(an_output_i32, IntLit(1)),
+                         Lt(an_output_i32, IntLit(1)),
+                         Eq(an_output_i32, IntLit(1)))
+        loop_cond = Choose(Le(an_output_i32, ml_list_length(an_input)),
+                           Lt(an_output_i32, ml_list_length(an_input)),
+                           Ge(an_output_i32, ml_list_length(an_input)),
+                           Gt(an_output_i32, ml_list_length(an_input)),
+                           Eq(an_output_i32, ml_list_length(an_input)))
+        conv = an_output_list
+        take_idx = Choose(an_output_i32, Sub(an_output_i32, IntLit(1)), Add(an_output_i32, IntLit(1)))
+        post = Eq(conv, ml_conv1d1x2(ml_list_take(an_input, an_output_i32), an_input))
+        summary = And(initial, And(loop_cond, post))
+        #prod = ci.modifiedVars[0]
+        #i = ci.modifiedVars[1]
+        #initial = Ge(i, IntLit(0))
+        #                 #Gt(i, IntLit(0)),
+        #                 #Le(i, IntLit(0)),
+        #                 #Lt(i, IntLit(0)),
+        #                 #Eq(i, IntLit(0)),
+        #                 #Ge(i, IntLit(1)),
+        #                 #Gt(i, IntLit(1)),
+        #                 #Le(i, IntLit(1)),
+        #                 #Lt(i, IntLit(1)),
+        #                 #Eq(i, IntLit(1)))
+        #loop_cond = Le(i, ml_list_length(some_input))
+        #                   #Le(i, ml_list_length(an_input)),
+        #                   #Gt(i, ml_list_length(an_input)),
+        #                   #Ge(i, ml_list_length(an_input)),
+        #                   #Eq(i, ml_list_length(an_input)))
+        #post = Eq(prod, ml_mul1d(ml_list_take(some_input, i), ml_list_take(other_input, i)))
+        #inv = And(initial, loop_cond)
+        #summary = And(inv, post)
+        #summary = BoolLit(True)
+        return Synth(name, summary, *ci.modifiedVars, *ci.readVars)
+    else:
+        output = ci.modifiedVars[0]
+        x = ci.readVars[0]
+        unknown_const = Choose(IntLit(0), IntLit(1), IntLit(2), IntLit(3))
+        y = ml_list_prepend(unknown_const, ml_list_prepend(unknown_const, ml_list_empty()))
+        # change this to Implies
+        #summary = Implies(ml_same_len(x, y), Eq(ml_mul1d(x, y), output))
+        summary = Eq(ml_conv1d1x2(x, y), output)
+        return Synth(name, summary, *ci.modifiedVars, *ci.readVars)
+
+def targetLang():
+    x = Var("x", ListT(Int()))
+    y = Var("y", ListT(Int()))
+
+    # Ignores the rest of x if y is shorter
+    def dotprod2d_body(x, y):
+        element1 = Mul(ml_list_head(x), ml_list_head(y))
+        x_rest = ml_list_tail(x, IntLit(1))
+        y_rest = ml_list_tail(y, IntLit(1))
+        element2 = Mul(ml_list_head(x_rest), ml_list_head(y_rest))
+        return Add(element1, element2)
+    dotprod2d = FnDeclNonRecursive(DOTPROD2D, Int(), dotprod2d_body(x, y), x, y)
+
+    def conv1d1x2_body(vec, kernel):
+        vec_size = ml_list_length(x)
+        kernel_size = IntLit(2)
+        cur_prod = ml_dotprod2d(vec, kernel)
+        vec_rest = ml_list_tail(vec, IntLit(1))
+        recursed = ml_conv1d1x2(vec_rest, kernel)
+        general_answer = ml_list_prepend(cur_prod, recursed)
+        return Ite(Eq(vec_size, kernel_size), ml_list_prepend(cur_prod, ml_list_empty()), general_answer)
+    conv1d1x2 = FnDecl(CONV1D1X2, ListT(Int()), conv1d1x2_body(x, y), x, y)
+
+    return [dotprod2d, conv1d1x2]
+
+basename = "conv1d1x2"
+filename = "tests/conv1d1x2.ll"
+fnName = "test"
+loopsFile = "tests/conv1d1x2.loops"
+cvcPath = "cvc5"
+
+(vars, invAndPs, preds, vc, loopAndPsInfo) = analyze(filename, fnName, loopsFile)
+
+invAndPs = [grammar(ci) for ci in loopAndPsInfo]
+lang = targetLang()
+
+candidates = synthesize(basename, lang, vars, invAndPs, preds, vc, loopAndPsInfo, cvcPath)
+
+def codeGen(summary: FnDecl):
+    expr = summary.body() 
+    def eval(expr):
+        if isinstance(expr, Eq):
+            return f"ans = {eval(expr.e2())}"
+        elif isinstance(expr, Add):
+            return f"{eval(expr.args[0])} + {eval(expr.args[1])}"
+        elif isinstance(expr, Call):
+            eval_args = []
+            for a in expr.arguments():
+                eval_args.append(eval(a))
+            name = expr.name()
+            return f"{name}({', '.join(eval_args)})"
+        elif isinstance(expr, Lit):
+            return str(expr.val())
+        elif isinstance(expr, Tuple):
+            eval_args = map(lambda expr: eval(expr), expr.args)
+            return f"[{', '.join(eval_args)}]"
+        else:
+            return str(expr)
+    return eval(expr)
+
+for c in candidates:
+    print(codeGen(c), "\n")
