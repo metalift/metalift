0c4e8fc062c75d650f7b7b23a0230318740b84dd
Yuto Nishida <yuto@berkeley.edu>
2023-04-10 03:27:03 -0700

    Increase synthesis size

--- tests/conv1d1x2const.py
+++ tests/conv1d1x2const.py
@@ -4,6 +4,7 @@ import shutil
 
 from metalift.ir import *
 from metalift.analysis import CodeInfo, analyze
+from metalift.synthesis_common import SynthesisFailed
 
 from metalift.synthesize_auto import synthesize
 
@@ -11,6 +12,7 @@ MUL1D = "elementwise_mul"
 SAME_LEN = "same_length"
 CONV1D1X2 = "conv1d1x2"
 DOTPROD2D = "dotprod_2d"
+DOTPROD = "dotprod"
 
 def ml_list_get(lst, i):
     return Call("list_get", Int(), lst, i)
@@ -38,6 +40,9 @@ def ml_min(a, b):
 
 def ml_dotprod2d(x, y):
     return Call(DOTPROD2D, Int(), x, y)
+
+def ml_dotprod(x, y):
+    return Call(DOTPROD, Int(), x, y)
 
 def ml_conv1d1x2(vec, kernel):
     return Call(CONV1D1X2, ListT(Int()), vec, kernel)
@@ -114,7 +119,7 @@ def grammar(ci: CodeInfo):
         an_input = ci.readVars[0]
         an_output = Choose(*ci.modifiedVars)
         x = ci.readVars[0]
-        unknown_const = Choose(IntLit(0), IntLit(1), IntLit(2), IntLit(3))
+        unknown_const = Choose(IntLit(0), IntLit(5), IntLit(2), IntLit(3))
         y = ml_list_prepend(unknown_const, ml_list_prepend(unknown_const, ml_list_empty()))
         # change this to Implies
         #summary = Implies(ml_same_len(x, y), Eq(ml_mul1d(x, y), output))
@@ -130,7 +135,7 @@ def grammar(ci: CodeInfo):
         summary = Implies(valid, check_ans)
         return Synth(name, summary, *ci.modifiedVars, *ci.readVars)
 
-def targetLang():
+def targetLang(kernel_size=2):
     x = Var("x", ListT(Int()))
     y = Var("y", ListT(Int()))
 
@@ -144,12 +149,22 @@ def targetLang():
         return Add(element1, element2)
     dotprod2d = FnDeclNonRecursive(DOTPROD2D, Int(), dotprod2d_body(x, y), x, y)
 
+    def dotprod_body(x, y):
+        kernel_size = ml_list_length(y)
+        cur_prod = Mul(ml_list_head(x), ml_list_head(y))
+        x_rest = ml_list_tail(x, IntLit(1))
+        y_rest = ml_list_tail(y, IntLit(1))
+        recursed = ml_dotprod(x_rest, y_rest)
+        return Ite(Lt(kernel_size, IntLit(2)), cur_prod, Add(cur_prod, recursed))
+    dotprod = FnDecl(DOTPROD, Int(), dotprod_body(x, y))
+
     # TODO: handle input size < 2
     # TODO: for size < 2, don't call dotprod
     def conv1d1x2_body(vec, kernel):
+        nonlocal kernel_size
         vec_size = ml_list_length(x)
-        kernel_size = IntLit(2)
-        cur_prod = ml_dotprod2d(vec, kernel)
+        kernel_size = IntLit(kernel_size)
+        cur_prod = ml_dotprod(vec, kernel)
         vec_rest = ml_list_tail(vec, IntLit(1))
         recursed = ml_conv1d1x2(vec_rest, kernel)
         general_answer = ml_list_prepend(cur_prod, recursed)
@@ -157,7 +172,7 @@ def targetLang():
         return Ite(Lt(vec_size, kernel_size), ml_list_empty(), general_answer)
     conv1d1x2 = FnDecl(CONV1D1X2, ListT(Int()), conv1d1x2_body(x, y), x, y)
 
-    return [dotprod2d, conv1d1x2]
+    return [dotprod2d, dotprod, conv1d1x2]
 
 def codeGen(summary: FnDecl):
     def eval(expr):
@@ -214,7 +229,13 @@ def runner():
     lang = targetLang()
 
     # noVerify=True is OK, since synthesis will not create a candidate for kernel that's too small
-    candidates = synthesize(basename, lang, vars, invAndPs, preds, vc, loopAndPsInfo, cvcPath, noVerify=True)
+    candidates = []
+    try:
+        candidates = synthesize(basename, lang, vars, invAndPs, preds, vc, loopAndPsInfo, cvcPath, noVerify=True)
+    except SynthesisFailed:
+        print("Synthesis failed")
+    except:
+        print("unexpected error")
 
     for c in candidates:
         if c.args[0] != "test":
