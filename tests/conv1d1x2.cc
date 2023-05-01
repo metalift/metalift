ae441df386cc8d6edf4db755f104817af4403742
Yuto Nishida <yuto@berkeley.edu>
2023-04-30 18:18:02 -0700

    Clean up C source

--- tests/conv1d1x2.cc
+++ tests/conv1d1x2.cc
@@ -3,30 +3,11 @@
 #include <string>
 #include "list.h"
 
-// Assume kernel has length 2
-//extern "C" List<int> test(List<int> vec, List<int> kernel)
-//{
-//  int slides = listLength(vec) - 1;
-//  List<int> convolved = newList<int>();
-//  for (int i = 0; i < slides; i++) {
-//    //int element1 = listGet(vec, i) * listGet(kernel, 0);
-//    //int element2 = listGet(vec, i + 1) * listGet(kernel, 1);
-//    //int dot_product = element1 + element2;
-//    //convolved = listAppend(convolved, dot_product);
-//    convolved = listAppend(convolved, listGet(vec, i) * listGet(kernel, 0) + listGet(vec, i+1) * listGet(kernel, 1));
-//  }
-//
-//  return convolved;
-//}
 extern "C" List<int> test(List<int> vec)
 {
   int slides = listLength(vec) - 1;
   List<int> convolved = newList<int>();
   for (int i = 0; i < slides; i++) {
-    //int element1 = listGet(vec, i) * listGet(kernel, 0);
-    //int element2 = listGet(vec, i + 1) * listGet(kernel, 1);
-    //int dot_product = element1 + element2;
-    //convolved = listAppend(convolved, dot_product);
     convolved = listAppend(convolved, listGet(vec, i) + listGet(vec, i+1));
   }
 
