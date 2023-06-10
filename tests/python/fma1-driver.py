54019f7505d7350c1b9fce66f9d88fd736a24874
Jie Qiu <jieqiu0630@gmail.com>
2023-06-10 18:06:17 -0400

    Add codegen for tuples

--- tests/python/fma1-driver.py
+++ tests/python/fma1-driver.py
@@ -5,6 +5,8 @@ from metalift.ir import Add, Call, Choose, Eq, Expr, FnDecl, Int, IntLit, Lit, V
 
 from mypy.nodes import Statement, WhileStmt
 
+from tests.python.utils.utils import codegen
+
 
 def target_lang() -> List[FnDecl]:
     x = Var("x", Int())
@@ -29,24 +31,6 @@ def ps_grammar(ret_val: Var, ast: Statement, writes: List[Var], reads: List[Var]
 
 def inv_grammar(v: Var, ast: Statement, writes: List[Var], reads: List[Var], in_scope: List[Var]) -> Expr:
     raise Exception("no loop in the source")
-    
-def codegen(expr: Expr) -> str:
-    def eval(expr: Expr) -> str:
-        if isinstance(expr, Eq):
-            return "%s = %s"%(expr.e1(), eval(expr.e2()))
-        if isinstance(expr, Add):
-            return "%s + %s"%(eval(expr.args[0]), eval(expr.args[1]))
-        if isinstance(expr, Call):
-            eval_args = []
-            for a in expr.arguments():
-                eval_args.append(eval(a))
-            return "%s(%s)"%(expr.name(), ', '.join(a for a in eval_args))
-        if isinstance(expr, Lit):
-            return "%s"%(expr.val())
-        else:
-            return "%s"%(expr)
-    return eval(expr)
-
 
 
 if __name__ == "__main__":
--- tests/python/utils/utils.py
+++ tests/python/utils/utils.py
@@ -1,19 +1,21 @@
-from metalift.ir import Add, Call, Eq, Expr, Lit
+from metalift.ir import Add, Call, Eq, Expr, Lit, Tuple
 
 
 def codegen(expr: Expr) -> str:
     def eval(expr: Expr) -> str:
         if isinstance(expr, Eq):
-            return "%s = %s"%(expr.e1(), eval(expr.e2()))
+            return f"{expr.e1()} = {eval(expr.e2())}"
         if isinstance(expr, Add):
-            return "%s + %s"%(eval(expr.args[0]), eval(expr.args[1]))
+            return f"{eval(expr.args[0])} + {eval(expr.args[1])}"
         if isinstance(expr, Call):
             eval_args = []
             for a in expr.arguments():
                 eval_args.append(eval(a))
-            return "%s(%s)"%(expr.name(), ', '.join(a for a in eval_args))
+            return f"{expr.name()}({', '.join(a for a in eval_args)})"
         if isinstance(expr, Lit):
-            return "%s"%(expr.val())
+            return f"{expr.val()}"
+        if isinstance(expr, Tuple):
+            return f"({', '.join(eval(a) for a in expr.args)})"
         else:
             return "%s"%(expr)
     return eval(expr)
