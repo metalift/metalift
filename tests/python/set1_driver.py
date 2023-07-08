6975a74e56b97c4e5a0465fe23d5e21d27d6d54d
Jie Qiu <jieqiu0630@gmail.com>
2023-07-08 15:52:19 -0400

    Add set

--- tests/python/set1_driver.py
+++ tests/python/set1_driver.py
@@ -1,10 +1,10 @@
 from typing import List
+
+from mypy.nodes import Statement
+
 from metalift.frontend.python import Driver
-
-from metalift.ir import Add, Call, Choose, Eq, Expr, FnDecl, Int, IntLit, Ite, Lit, SetT, Var
-
-from mypy.nodes import Statement, WhileStmt
-
+from metalift.ir import (Call, Choose, Eq, Expr, FnDecl, Int, IntLit, Ite,
+                         SetT, Var)
 from tests.python.utils.utils import codegen
 
 
@@ -13,22 +13,19 @@ def target_lang() -> List[FnDecl]:
 
 def ps_grammar(ret_val: Var, ast: Statement, writes: List[Var], reads: List[Var], in_scope: List[Var]) -> Expr:
     input_s, input_add, input_value = reads
-
-    empty_set = Call("set-create", SetT(Int()))
-
     int_lit = Choose(IntLit(0), IntLit(1), IntLit(2), IntLit(3))
     int_value = Choose(input_value, int_lit)
 
     condition = Eq(input_add, int_lit)
 
+    empty_set = Call("set-create", SetT(Int()))
     set_in = Choose(input_s, empty_set, Call("set-singleton", SetT(Int()), int_value))
-
     set_transform = Choose(
         set_in,
         Call("set-union", SetT(Int()), set_in, set_in),
         Call("set-minus", SetT(Int()), set_in, set_in),
     )
-    return Ite(condition, set_transform, set_transform)
+    return Eq(ret_val, Ite(condition, set_transform, set_transform))
 
 def inv_grammar(v: Var, ast: Statement, writes: List[Var], reads: List[Var], in_scope: List[Var]) -> Expr:
     raise Exception("no loop in the source")
