1b3f9973f0d38344346c46908bd6586b3f4441bb
Colin Cai <colincai09@gmail.com>
2023-10-15 22:23:46 -0700

    fix for python ir.util

--- metalift/ir_util.py
+++ metalift/ir_util.py
@@ -2,8 +2,8 @@ from metalift.ir import Expr
 
 
 def is_list_type_expr(expr: Expr) -> bool:
-    return expr.type.name == "MLList"
+    return expr.type.cls_str() == "List"
 
 
 def is_set_type_expr(expr: Expr) -> bool:
-    return expr.type.name == "Set"
+    return expr.type.cls_str() == "Set"
