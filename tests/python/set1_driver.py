26a50cc40ec0fa14108e8aec1b159635af1fd0e8
Jie Qiu <jieqiu0630@gmail.com>
2023-10-17 08:13:38 -0700

    set1 works

--- tests/python/set1_driver.py
+++ tests/python/set1_driver.py
@@ -1,59 +1,68 @@
 from typing import List
 
 from metalift.frontend.python import Driver
-from metalift.ir import (Bool, Int, Object, Set as mlSet,
-                         choose, ite, fn_decl_recursive)
+from metalift.ir import (Add, Call, Choose, Expr, IntObject, NewObject, SetObject,
+                         FnDeclRecursive, Ite)
 from tests.python.utils.utils import codegen
 
+def double(t):
+    return IntObject(Call("double", IntObject, t))
 
 def target_lang():
-    x = Int("x")
-    double = fn_decl_recursive(
+    x = IntObject("x")
+    double = FnDeclRecursive(
         "double",
-        Int,
-        (x + x),
+        IntObject,
+        x + x,
         x
     )
     return [double]
 
-def inv_grammar(writes: List[Object], reads: List[Object], in_scope: List[Object]) -> Bool:
+def inv_grammar(v: NewObject, writes: List[NewObject], reads: List[NewObject], in_scope: List[NewObject]) -> Expr:
     raise Exception("no invariant")
 
-def ps_grammar(writes: List[Object], reads: List[Object], in_scope: List[Object]) -> Bool:
-    ret_val = writes[0]
+def ps_grammar(ret_val: NewObject, writes: List[NewObject], reads: List[NewObject], in_scope: List[NewObject]) -> Expr:
     input_s = reads[0]
     input_add = reads[1]
     input_value = reads[2]
     output_var = writes[0]
 
-    empty_set = mlSet.empty(Int)
+    empty_set = SetObject.empty(IntObject)
 
-    int_lit = choose(
-        Int(0),
-        Int(1),
-        Int(2),
-        Int(3)
+    int_lit = IntObject(
+        Choose(
+            IntObject(0),
+            IntObject(1),
+            IntObject(2),
+            IntObject(3)
+        )
     )
-    int_value = choose(input_value, int_lit)
+
+    int_value = IntObject(Choose(input_value, int_lit))
 
     condition = input_add == int_lit
 
-    set_in = choose(
-        input_s,
-        empty_set,
-        mlSet.singleton(int_value)
+    set_in = SetObject[IntObject](
+        IntObject,
+        Choose(
+            input_s,
+            empty_set,
+            SetObject.singleton(int_value)
+        )
     )
 
-    set_transform = choose(
-        set_in,
-        set_in.union(set_in),
-        set_in.difference(set_in)
+    set_transform = SetObject[IntObject](
+        IntObject,
+        Choose(
+            set_in,
+            set_in.union(set_in),
+            set_in.difference(set_in)
+        )
     )
 
-    chosen_transform = ite(
-        condition,
-        set_transform,
-        set_transform
+    chosen_transform = SetObject[IntObject](
+        IntObject,
+        Ite(condition, set_transform, set_transform)
     )
 
     summary = output_var == chosen_transform
@@ -69,9 +78,9 @@ if __name__ == "__main__":
         ps_grammar=ps_grammar
     )
 
-    s = mlSet[Int](Int, "s")
-    add = Int("add")
-    value = Int("value")
+    s = SetObject[IntObject](IntObject, "s")
+    add = IntObject("add")
+    value = IntObject("value")
     driver.add_var_objects([s, add, value])
 
     test(s, add, value)
