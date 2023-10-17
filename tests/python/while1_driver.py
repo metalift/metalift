6be68abdeef2750f9d1c66dbe5cd2fa03b00760b
Jie Qiu <jieqiu0630@gmail.com>
2023-10-16 21:00:45 -0700

    while1 passes

--- tests/python/while1_driver.py
+++ tests/python/while1_driver.py
@@ -1,3 +1,8 @@
+from typing import List
+from metalift.frontend.python import Driver
+
+from metalift.ir import Eq, Expr, FnDecl, IntObject, Ite, Le, NewObject
+
 import sys
 from typing import List
 
@@ -7,22 +12,23 @@ from metalift.ir import Bool, FnDecl, Int, Object, ite
 
 # ps: y = ite(y<=x, x, 0)
 def ps_grammar(
-    writes: List[Object],
-    reads: List[Object],
-    in_scope: List[Object],
-) -> Bool:
-    ret_val = writes[0]
+    ret_val: NewObject,
+    writes: List[NewObject],
+    reads: List[NewObject],
+    in_scope: List[NewObject],
+) -> Expr:
     x = reads[0]
-    return ret_val == ite(0 <= x, x, Int(0))
+    return Eq(ret_val, Ite(0 <= x, x, IntObject(0)))
 
 
-# inv: ite(y<=x, 0<=x, y=0)
-def inv_grammar(writes: List[Object], reads: List[Object], in_scope: List[Object]) -> Bool:
-    x, y = reads
-    return ite(
-        y <= x,
-        0 <= x,
-        y == 0
+# inv: ite(y<=x, y<=x, y=0)
+def inv_grammar(
+    v: NewObject, writes: List[NewObject], reads: List[NewObject], in_scope: List[NewObject]
+) -> Expr:
+    return Ite(
+        v <= reads[0],
+        0 <= reads[0],
+        v == 0
     )
 
 
@@ -36,7 +42,7 @@ if __name__ == "__main__":
     driver = Driver()
     test = driver.analyze(filename, "test", target, inv_grammar, ps_grammar)
 
-    x = Int("x")
+    x = IntObject("x")
     driver.add_var_object(x)
     r = test(x)
 
