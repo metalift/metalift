db16239803f0fde62188de95bc1b3f09cef9d68a
Jie Qiu <jieqiu0630@gmail.com>
2023-10-24 11:09:48 -0400

    set1 passes

--- tests/llvm/set1_driver.py
+++ tests/llvm/set1_driver.py
@@ -3,26 +3,23 @@ from typing import List
 
 from metalift.frontend.llvm import Driver
 from metalift.ir import (Add, Call, Choose, Expr, IntObject, NewObject, SetObject,
-                         FnDeclRecursive, Ite)
+                         FnDeclRecursive, Ite, call, choose, ite)
 from tests.python.utils.utils import codegen
-
-def double(t):
-    return IntObject(Call("double", IntObject, t))
 
 def target_lang():
     x = IntObject("x")
     double = FnDeclRecursive(
         "double",
         IntObject,
-        x + x,
-        x
+        (x + x).src,
+        x.src
     )
     return [double]
 
 def inv_grammar(v: NewObject, writes: List[NewObject], reads: List[NewObject]) -> Expr:
     raise Exception("no invariant")
 
-def ps_grammar(ret_val: NewObject, writes: List[NewObject], reads: List[NewObject]) -> Expr:
+def ps_grammar(ret_val: NewObject, writes: List[NewObject], reads: List[NewObject]) -> NewObject:
     input_s = reads[0]
     input_add = reads[1]
     input_value = reads[2]
@@ -30,40 +27,32 @@ def ps_grammar(ret_val: NewObject, writes: List[NewObject], reads: List[NewObjec
 
     empty_set = SetObject.empty(IntObject)
 
-    int_lit = IntObject(
-        Choose(
-            IntObject(0),
-            IntObject(1),
-            IntObject(2),
-            IntObject(3)
-        )
+    int_lit = choose(
+        IntObject(0),
+        IntObject(1),
+        IntObject(2),
+        IntObject(3)
     )
-
-    int_value = IntObject(Choose(input_value, int_lit))
+    int_value = choose(input_value, int_lit)
 
     condition = input_add == int_lit
 
-    set_in = SetObject[IntObject](
-        IntObject,
-        Choose(
-            input_s,
-            empty_set,
-            SetObject.singleton(int_value)
-        )
+    set_in = choose(
+        input_s,
+        empty_set,
+        SetObject.singleton(int_value)
     )
 
-    set_transform = SetObject[IntObject](
-        IntObject,
-        Choose(
-            set_in,
-            set_in.union(set_in),
-            set_in.difference(set_in)
-        )
+    set_transform = choose(
+        set_in,
+        set_in.union(set_in),
+        set_in.difference(set_in)
     )
 
-    chosen_transform = SetObject[IntObject](
-        IntObject,
-        Ite(condition, set_transform, set_transform)
+    chosen_transform = ite(
+        condition,
+        set_transform,
+        set_transform
     )
 
     summary = output_var == chosen_transform
@@ -80,7 +69,7 @@ if __name__ == "__main__":
         ps_grammar=ps_grammar
     )
 
-    s = SetObject[IntObject](IntObject, "s")
+    s = SetObject(IntObject, "s")
     add = IntObject("add")
     value = IntObject("value")
     driver.add_var_objects([s, add, value])
