01ddfc9e234977fe1928d09c062e5f1943a724d6
Jie Qiu <jieqiu0630@gmail.com>
2023-10-29 18:10:25 -0400

    test_cblas_sgemv runs but synthesis fails

--- tests/llvm/test_cblas_sgemv_driver.py
+++ tests/llvm/test_cblas_sgemv_driver.py
@@ -4,13 +4,28 @@ from metalift.ir import FnDecl, FnDeclRecursive, IntObject, ListObject, NewObjec
 from metalift.vc_util import and_objects
 from tests.python.utils.utils import codegen
 
+def cblas_sgemv(
+    alpha: IntObject,
+    a: ListObject[ListObject[IntObject]],
+    x: IntObject,
+    beta: IntObject,
+    y: IntObject
+) -> ListObject[IntObject]:
+    return call("cblas_sgemv", ListObject[IntObject], alpha, a, x, beta, y)
+
+def sdot(x: ListObject[IntObject], y: ListObject[IntObject]) -> IntObject:
+    return call("sdot", IntObject, x, y)
+
+def sgemv(a: ListObject[ListObject[IntObject]], x: ListObject[IntObject]) -> ListObject[IntObject]:
+    return call("sgemv", ListObject[IntObject], a, x)
+
 def target_lang() -> List[Union[FnDecl, FnDeclRecursive]]:
     x = ListObject(IntObject, "x")
     y = ListObject(IntObject, "y")
     sdot_cond = and_objects(x.len() > 0, y.len() > 0, x.len() == y.len())
-    sdot_then = x[0] * y[0] + call("sdot", x[1:], y[1:])
+    sdot_then = x[0] * y[0] + sdot(x[1:], y[1:])
     sdot_else = IntObject(0)
-    sdot = FnDeclRecursive(
+    sdot_decl = FnDeclRecursive(
         "sdot",
         IntObject,
         ite(sdot_cond, sdot_then, sdot_else).src,
@@ -21,9 +36,9 @@ def target_lang() -> List[Union[FnDecl, FnDeclRecursive]]:
     a = ListObject(ListObject[IntObject], "a")
     x = ListObject(IntObject, "x")
     sgemv_cond = x.len() == a[0].len()
-    sgemv_then = call("sgemv", a[1:], x).prepend(call("sdot", a[0], x))
+    sgemv_then = sgemv(a[1:], x).prepend(sdot(a[0], x))
     sgemv_else = ListObject.empty(IntObject)
-    sgemv = FnDeclRecursive(
+    sgemv_decl = FnDeclRecursive(
         "sgemv",
         ListObject[IntObject],
         ite(sgemv_cond, sgemv_then, sgemv_else).src,
@@ -42,9 +57,9 @@ def target_lang() -> List[Union[FnDecl, FnDeclRecursive]]:
         a[0].len() == x.len(),
         a.len() == y.len()
     )
-    cblas_sgemv_else = call("cblas_sgemv", alpha, a[1:], x, beta, y[1:]).prepend(alpha * call("sdot", a[0], x) + beta * y[0])
+    cblas_sgemv_else = cblas_sgemv(alpha, a[1:], x, beta, y[1:]).prepend(alpha * sdot(a[0], x) + beta * y[0])
     cblas_sgemv_then = ListObject.empty(IntObject)
-    cblas_sgemv = FnDeclRecursive(
+    cblas_sgemv_decl = FnDeclRecursive(
         "cblas_sgemv",
         ListObject[IntObject],
         ite(cblas_sgemv_cond, cblas_sgemv_then, cblas_sgemv_else).src,
@@ -54,34 +69,42 @@ def target_lang() -> List[Union[FnDecl, FnDeclRecursive]]:
         beta.src,
         y.src,
     )
-    return [cblas_sgemv]
+    return [sdot_decl, sgemv_decl, cblas_sgemv_decl]
 
-def inv0_grammar(writes: List[NewObject], reads: List[NewObject]) -> NewObject:
-    res, j, z, i = writes
+def inv0_grammar(writes: List[NewObject], reads: List[NewObject], in_scope: List[NewObject]) -> NewObject:
+    # Outer loop
+    z, res, j, _, i = writes
     alpha, a, x, beta, y = reads
 
-    and_objects(
+    return and_objects(
         i >= 0,
         i <= a.len(),
-        z == call("cblas_sgemv", alpha, a[:i], x, beta, y[:i])
+        z == cblas_sgemv(alpha, a[:i], x, beta, y[:i])
     )
 
-def inv1_grammar(writes: List[NewObject], reads: List[NewObject]) -> NewObject:
-    res, j, z, i = writes
+def inv1_grammar(writes: List[NewObject], reads: List[NewObject], in_scope: List[NewObject]) -> NewObject:
+    # Inner loop
+    res, j = writes
     alpha, a, x, beta, y = reads
+    in_scope_mapping = {
+        obj.var_name(): obj
+        for obj in in_scope
+    }
+    i = in_scope_mapping["i"]
+    z = in_scope_mapping["agg.result"]
     return and_objects(
         j >= 0,
         j <= x.len(),
         i >= 0,
         i < a.len(),
-        res == call("sdot", a[i][:j], x[:j]),
-        z == call("cblas_sgemv", alpha, a[:i], x, beta, y[:i])
+        res == sdot(a[i][:j], x[:j]),
+        z == cblas_sgemv(alpha, a[:i], x, beta, y[:i])
     )
 
-def ps_grammar(writes: List[NewObject], reads: List[NewObject]) -> NewObject:
+def ps_grammar(writes: List[NewObject], reads: List[NewObject], in_scope: List[NewObject]) -> NewObject:
     ret_val = writes[0]
     alpha, a, x, beta, y = reads
-    return ret_val == call("cblas_sgemv", alpha, a, x, beta, y)
+    return ret_val == cblas_sgemv(alpha, a, x, beta, y)
 
 if __name__ == "__main__":
     driver = Driver()
@@ -90,7 +113,10 @@ if __name__ == "__main__":
         loops_filepath="tests/llvm/test_cblas_sgemv.loops",
         fn_name="test_cblas_sgemv",
         target_lang_fn=target_lang,
-        inv_grammar=inv1_grammar,
+        inv_grammars={
+            "test_cblas_sgemv_inv0": inv0_grammar,
+            "test_cblas_sgemv_inv1": inv1_grammar
+        },
         ps_grammar=ps_grammar
     )
 
