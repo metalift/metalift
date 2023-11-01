008484d275d45e7ce2f7507b4737c76583b7a1be
Colin Cai <colincai09@gmail.com>
2023-10-31 18:43:35 -0700

    python list1 passing

--- tests/llvm/no_loop_matmul_driver.py
+++ tests/llvm/no_loop_matmul_driver.py
@@ -3,56 +3,58 @@ from typing import List
 from mypy.nodes import Statement
 
 from metalift.frontend.llvm import Driver
-from metalift.ir import (Add, Call, Choose, Eq, Expr, FnDecl, FnDeclRecursive,
-                         Int, IntLit, IntObject, Ite, Lt, Mul, Sub, Tuple, TupleGet,
-                         TupleT, Var)
+from metalift.ir import (NewObject, Expr, FnDeclRecursive,
+                         IntObject, Tuple, TupleObject, make_tuple, make_tuple_type, fnDecl, ite, call, choose)
 from tests.python.utils.utils import codegen
 
 L1_NORM = "l1_norm"
 MAT_MUL = "mat_mul"
 
 def target_lang() -> List[FnDeclRecursive]:
-    a = Var("a", TupleT(Int(), Int()))
-    b = Var("b", TupleT(Int(), Int()))
-    x = Var("x", TupleT(Int(), Int()))
-    p0l = Mul(TupleGet(a, IntLit(0)), TupleGet(x, IntLit(0)))
-    p0r = Mul(TupleGet(b, IntLit(0)), TupleGet(x, IntLit(1)))
-    p1l = Mul(TupleGet(a, IntLit(1)), TupleGet(x, IntLit(0)))
-    p1r = Mul(TupleGet(b, IntLit(1)), TupleGet(x, IntLit(1)))
-    mat_mul_body = Tuple(Add(p0l, p0r), Add(p1l, p1r))
-    mat_mul = FnDecl(MAT_MUL, TupleT(Int(), Int()), mat_mul_body, a, b, x)
+    a = TupleObject(IntObject, IntObject, "a")
+    b = TupleObject(IntObject, IntObject, "b")
+    x = TupleObject(IntObject, IntObject, "x")
+    p0l = a[0] * x[0] 
+    p0r = b[0] * x[1] 
+    p1l = a[1] * x[0]
+    p1r = b[1] * x[1] 
+    mat_mul_body = make_tuple(p0l + p0r, p1l + p1r)
+    mat_mul = fnDecl(MAT_MUL, make_tuple_type(IntObject, IntObject), mat_mul_body, a, b, x)
 
-    p = Var("p", TupleT(Int(), Int()))
-    p0 = TupleGet(p, IntLit(0))
-    p1 = TupleGet(p, IntLit(1))
-    p0_abs = Ite(Lt(p0, IntLit(0)), Sub(IntLit(0), p0), p0)
-    p1_abs = Ite(Lt(p1, IntLit(0)), Sub(IntLit(0), p1), p1)
-    l1_norm_body = Add(p0_abs, p1_abs)
-    l1_norm = FnDecl(L1_NORM, Int(), l1_norm_body, p)
+    p = TupleObject(IntObject, IntObject, "p")
+    p0 = p[0]
+    p1 = p[1]
+    p0_abs = ite(p0 < 0,0 - p0, p0)
+    p1_abs = ite(p1 < 0, 0 - p1, p1)
+    l1_norm_body = p0_abs and p1_abs
+    l1_norm = fnDecl(L1_NORM, IntObject, l1_norm_body, p)
 
     return [mat_mul, l1_norm]
 
 
-def ps_grammar(ret_val: Var, writes: List[Var], reads: List[Var]) -> Expr:
+def ps_grammar(ret_val: NewObject, writes: List[NewObject], reads: List[NewObject]) -> Expr:
     a0, a1, b0, b1, x0, x1 = reads
     # Calculate the matrix-vector product
-    a = Tuple(a0, a1)
-    b = Tuple(b0, b1)
-    x = Tuple(x0, x1)
-    p = Call(MAT_MUL, TupleT(Int(), Int()), a, b, x)
-    wrong_p = Call(MAT_MUL, TupleT(Int(), Int()), b, a, x)
-    wrong_p2 = Call(MAT_MUL, TupleT(Int(), Int()), a, x,  b)
+    a_src = Tuple(a0, a1)
+    a = TupleObject(IntObject, IntObject, a_src)
+    b_src = Tuple(b0, b1)
+    b = TupleObject(IntObject, IntObject, b_src)
+    x_src = Tuple(x0, x1)
+    x = TupleObject(IntObject, IntObject, x_src)
+    p = call(MAT_MUL, make_tuple_type(IntObject, IntObject), a, b, x)
+    wrong_p = call(MAT_MUL, make_tuple_type(IntObject, IntObject), b, a, x)
+    wrong_p2 = call(MAT_MUL, make_tuple_type(IntObject, IntObject), a, x, b)
 
     # this is the correct answer
-    l1_norm_p = Call(L1_NORM, Int(), p)
+    l1_norm_p = call(L1_NORM, IntObject, p)
     # this is a wrong answer
-    l1_norm_wrong_p = Call(L1_NORM, Int(), wrong_p)
+    l1_norm_wrong_p = call(L1_NORM, IntObject, wrong_p)
     # this is a wrong answer
-    l1_norm_wrong_p2 = Call(L1_NORM, Int(), wrong_p2)
+    l1_norm_wrong_p2 = call(L1_NORM, IntObject, wrong_p2)
 
-    return Eq(ret_val, Choose(l1_norm_p, l1_norm_wrong_p, l1_norm_wrong_p2))
+    return ret_val == choose(l1_norm_p, l1_norm_wrong_p, l1_norm_wrong_p2)
 
-def inv_grammar(v: Var, writes: List[Var], reads: List[Var]) -> Expr:
+def inv_grammar(v: NewObject, writes: List[NewObject], reads: List[NewObject]) -> Expr:
     raise Exception("no loop in the source")
 
 if __name__ == "__main__":
