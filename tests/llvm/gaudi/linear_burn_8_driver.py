21a6680dd716bc04859755926a3500d76bb6d39b
Jie Qiu <jieqiu0630@gmail.com>
2023-11-11 14:16:28 -0500

    Add more things

--- tests/llvm/gaudi/linear_burn_8_driver.py
+++ tests/llvm/gaudi/linear_burn_8_driver.py
@@ -1,13 +1,13 @@
 import time
 
 from metalift.frontend.llvm import Driver
-from metalift.ir import Int, List as mlList
-from tests.llvm.gaudi.gaudi_common import (all_possible_selects_two_args_synth,
-                                           selection_two_args_inv0_grammar,
-                                           selection_two_args_inv1_grammar,
-                                           selection_two_args_ps_grammar_fn,
-                                           selection_two_args_synth,
-                                           selection_two_args_target_lang)
+from metalift.ir import Int
+from metalift.ir import List as mlList
+from tests.llvm.gaudi.gaudi_common import (
+    all_possible_selects_two_args_synth, nested_list_computation_inv0_grammar,
+    nested_list_computation_inv1_grammar,
+    nested_list_computation_ps_grammar_fn, nested_list_computation_target_lang,
+    selection_two_args_synth)
 from tests.python.utils.utils import codegen
 
 if __name__ == "__main__":
@@ -16,12 +16,12 @@ if __name__ == "__main__":
         llvm_filepath="tests/llvm/gaudi/linear_burn_8.ll",
         loops_filepath="tests/llvm/gaudi/linear_burn_8.loops",
         fn_name="linear_burn_8",
-        target_lang_fn=selection_two_args_target_lang,
+        target_lang_fn=nested_list_computation_target_lang,
         inv_grammars={
-            "linear_burn_8_inv0": selection_two_args_inv0_grammar,
-            "linear_burn_8_inv1": selection_two_args_inv1_grammar
+            "linear_burn_8_inv0": nested_list_computation_inv0_grammar,
+            "linear_burn_8_inv1": nested_list_computation_inv1_grammar
         },
-        ps_grammar=selection_two_args_ps_grammar_fn
+        ps_grammar=nested_list_computation_ps_grammar_fn
     )
 
     base = mlList(mlList[Int], "base")
