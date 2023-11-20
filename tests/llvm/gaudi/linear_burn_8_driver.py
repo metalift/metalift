a688cd9bd34d6177d2348adf353dd5e386997f67
Jie Qiu <jieqiu0630@gmail.com>
2023-11-19 22:43:35 -0500

    Add analysis fixed inv list bound 2

--- tests/llvm/gaudi/linear_burn_8_driver.py
+++ tests/llvm/gaudi/linear_burn_8_driver.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     linear_burn_8(base, active)
 
     start_time = time.time()
-    driver.synthesize(listBound=3, noVerify=True)
+    driver.synthesize(listBound=2, noVerify=True)
     end_time = time.time()
     print(f"Synthesis took {end_time - start_time} seconds")
     print("\n\ngenerated code:" + linear_burn_8.codegen(codegen))
