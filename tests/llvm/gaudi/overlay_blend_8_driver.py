b6f8987f74ebe6ff4de73b71d019d39ac0156fc1
Jie Qiu <jieqiu0630@gmail.com>
2023-11-26 00:54:09 -0500

    Add count constraint for computational grammar

--- tests/llvm/gaudi/overlay_blend_8_driver.py
+++ tests/llvm/gaudi/overlay_blend_8_driver.py
@@ -1,9 +1,13 @@
+from collections import OrderedDict
 import time
 
 from metalift.frontend.llvm import Driver
-from metalift.ir import Int, Matrix
+from metalift.ir import Int, Matrix, synth
 from tests.llvm.gaudi.gaudi_common import (
+    FIXED_SELECT_TWO_ARGS,
+    get_multi_depth_with_counts_select_general_synth,
     get_select_two_args_synth_without_analysis,
+    select_overlay_blend_body,
     selection_two_args_inv0_grammar, selection_two_args_inv1_grammar,
     selection_two_args_ps_grammar_fn, selection_two_args_target_lang)
 from tests.python.utils.utils import codegen
@@ -34,12 +38,32 @@ if __name__ == "__main__":
 
     int_x = Int("int_x")
     int_y = Int("int_y")
-    select_synth = get_select_two_args_synth_without_analysis(5)
-    driver.fns_synths = [select_synth]
+    ordered_compute_ops = OrderedDict()
+    ordered_compute_ops["+"] = 1
+    ordered_compute_ops["-"] = 2
+    ordered_compute_ops["*"] = 5
+    ordered_compute_ops["//"] = 2
+    select_synth = get_multi_depth_with_counts_select_general_synth(
+        args=[int_x, int_y],
+        constants=[Int(2), Int(16), Int(32)],
+        ordered_compute_ops=ordered_compute_ops,
+        compare_ops=[">="],
+        cond_lhs_depth=0,
+        cond_rhs_depth=0,
+        if_then_depth=5,
+        if_else_depth=3
+    )
+    fixed_select_synth = synth(
+        FIXED_SELECT_TWO_ARGS,
+        select_overlay_blend_body(int_x, int_y),
+        int_x,
+        int_y
+    )
+    driver.fns_synths = [select_synth, fixed_select_synth]
     overlay_blend_8(base, active)
 
     start_time = time.time()
-    driver.synthesize(listBound=3, noVerify=True)
+    driver.synthesize(listBound=2, noVerify=True)
     end_time = time.time()
     print(f"Synthesis took {end_time - start_time} seconds")
     print("\n\ngenerated code:" + overlay_blend_8.codegen(codegen))
