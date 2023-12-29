364f72e4ec1fc4770c62b987565a4e259a53a2cf
Jie Qiu <jieqiu0630@gmail.com>
2023-12-29 01:12:02 -0500

    Add numba files

---
+++ tests/llvm/gaudi/benchmark/numba/multiply_blend_8.py
@@ -0,0 +1,13 @@
+from numba import njit
+
+@njit
+def multiply_blend_8(base, active):
+  output = []
+  m = len(base)
+  n = len(base[0])
+  for i in range(m):
+    curr_row = []
+    for j in range(n):
+      curr_row.append(base[i][j] * active[i][j] / 255)
+    output.append(curr_row)
+  return output
