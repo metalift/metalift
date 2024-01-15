6ef1bf9448a03adba5a819b5a09f34b73f394dee
Jie Qiu <jieqiu0630@gmail.com>
2024-01-15 01:43:18 -0500

    fix import

--- metalift/synthesize_auto.py
+++ metalift/synthesize_auto.py
@@ -1,7 +1,7 @@
 import os
 
-# isort: skip_file
+# pragma: no autoflake
 if os.environ.get("SYNTH_CVC5") == "1":
-    pass
+    from metalift.synthesize_cvc5 import synthesize  # noqa
 else:
-    pass
+    from metalift.synthesize_rosette import synthesize  # noqa
