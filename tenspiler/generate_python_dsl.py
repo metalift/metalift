b7ca34bf594a0186f4b5bb10996f796cd4916fc7
Jie Qiu <jieqiu0630@gmail.com>
2024-02-25 23:43:43 -0500

    Modify python dsl

--- tenspiler/generate_python_dsl.py
+++ tenspiler/generate_python_dsl.py
@@ -1,22 +1,19 @@
 from tenspiler.tenspiler_common import (
     MAP_INT_TO_INT,
-    matrix_matrix_to_matrix_target_lang,
-    matrix_vec_to_vec_target_lang,
-    scalar_matrix_to_matrix_target_lang,
-    scalar_vec_to_vec_target_lang,
-    vec_to_int_target_lang,
-    vec_to_vec_target_lang,
-    vec_vec_to_vec_target_lang,
+    matrix_selection_two_args_fn_decl,
+    selection_two_args_fn_decl,
 )
 
 for fn in [
-    *vec_to_int_target_lang,
-    *matrix_vec_to_vec_target_lang,
-    *vec_vec_to_vec_target_lang,
-    *matrix_matrix_to_matrix_target_lang,
-    *scalar_vec_to_vec_target_lang,
-    *scalar_matrix_to_matrix_target_lang,
-    *vec_to_vec_target_lang,
+    # *vec_to_int_target_lang,
+    # *matrix_vec_to_vec_target_lang,
+    # *vec_vec_to_vec_target_lang,
+    # *matrix_matrix_to_matrix_target_lang,
+    # *scalar_vec_to_vec_target_lang,
+    # *scalar_matrix_to_matrix_target_lang,
+    # *vec_to_vec_target_lang,
+    matrix_selection_two_args_fn_decl,
+    selection_two_args_fn_decl,
 ]:
     if fn.name() == MAP_INT_TO_INT:
         continue
