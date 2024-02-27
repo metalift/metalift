8999d775f9200d3f894a064f44bddf58fd53cac6
Jie Qiu <jieqiu0630@gmail.com>
2024-02-27 16:31:45 -0500

    Add llm numbers

--- tenspiler/benchmark/cpp/normal_blend_f.cc
+++ tenspiler/benchmark/cpp/normal_blend_f.cc
@@ -2,8 +2,6 @@
 #include <vector>
 #include <chrono>
 
-#include "utils.h"
-
 using namespace std;
 using namespace std::chrono;
 
@@ -14,9 +12,4 @@ vector<float> normal_blend_f(vector<float> base, vector<float> active, float opa
     out.push_back(opacity * active[i] + (1 - opacity) * base[i]);
 
   return out;
-}
-
-int main() {
-  vec_timer_float(&normal_blend_f);
-  return 0;
 }
