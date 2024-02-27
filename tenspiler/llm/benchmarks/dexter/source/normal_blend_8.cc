65ee1716b6f7cea9f8c94125900f60bc466baab6
Jie Qiu <jieqiu0630@gmail.com>
2024-02-27 16:31:45 -0500

    Add llm numbers

---
+++ tenspiler/llm/benchmarks/dexter/source/normal_blend_8.cc
@@ -0,0 +1,11 @@
+#include <vector>
+using namespace std;
+
+vector<int> normal_blend_8(vector<int> base, vector<int> active, int opacity)
+{
+  vector<int> out;
+  for (int i = 0; i < base.size(); ++i)
+    out.push_back(opacity * active[i] + (255 - opacity) * base[i]);
+
+  return out;
+}
---
+++ tenspiler/llm/benchmarks/dexter/source/overlay_blend_8.cc
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+vector<vector<int>> overlay_blend_8(vector<vector<int>> base, vector<vector<int>> active)
+{
+    vector<vector<int>> out;
+    int m = base.size();
+    int n = base[0].size();
+	for (int row = 0; row < m; row++) {
+        vector<int> row_vec;
+		for (int col = 0; col < n; col++) {
+			int pixel;
+			if (base[row][col] >= 128)
+                pixel = 2 * base[row][col] + base[row][col] - 2 * base[row][col] * base[row][col] / 255 - 255;
+			else
+                pixel = 2 * base[row][col] * base[row][col] / 255;
+			row_vec.push_back(pixel);
+		}
+		out.push_back(row_vec);
+	}
+	return out;
+}
