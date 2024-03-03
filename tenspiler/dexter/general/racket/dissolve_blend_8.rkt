ad8b2f2a7d6d7a6049089f917c8252d8eb57b88b
Jie Qiu <jieqiu0630@gmail.com>
2024-03-03 02:01:05 -0500

    Fix general dissolve blend 8 racket file

--- tenspiler/dexter/general/racket/dissolve_blend_8.rkt
+++ tenspiler/dexter/general/racket/dissolve_blend_8.rkt
@@ -7,6 +7,14 @@
 
 
 
+ (define-bounded (matrix_vec_mul matrix_x x)
+(if (or (or (< (list-list-length matrix_x ) 1 ) (< (length (list-list-ref-noerr matrix_x 0 ) ) 1 ) ) (! (equal? (length (list-list-ref-noerr matrix_x 0 ) ) (length x ) ) ) ) (list-empty ) (list-prepend (reduce_sum (vec_elemwise_mul (list-list-ref-noerr matrix_x 0 ) x)) (matrix_vec_mul (list-list-tail-noerr matrix_x 1 ) x ) ) ))
+
+
+ (define-bounded (reduce_sum x)
+(if (< (length x ) 1 ) 0 (+ (list-ref-noerr x 0 ) (reduce_sum (list-tail-noerr x 1 )) ) ))
+
+
  (define-bounded (dissolve_selection_two_args x y opacity rand_cons dissolve_select_two_args_arg)
 (if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (dissolve_select_two_args_arg (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) opacity rand_cons) (dissolve_selection_two_args (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) opacity rand_cons dissolve_select_two_args_arg) ) ))
 
@@ -14,45 +22,131 @@
  (define-bounded (dissolve_matrix_selection_two_args matrix_x matrix_y opacity rand_cons dissolve_select_two_args_arg)
 (if (or (< (list-list-length matrix_x ) 1 ) (! (equal? (list-list-length matrix_x ) (list-list-length matrix_y ) ) ) ) (list-list-empty ) (list-list-prepend (dissolve_selection_two_args (list-list-ref-noerr matrix_x 0 ) (list-list-ref-noerr matrix_y 0 ) opacity rand_cons dissolve_select_two_args_arg) (dissolve_matrix_selection_two_args (list-list-tail-noerr matrix_x 1 ) (list-list-tail-noerr matrix_y 1 ) opacity rand_cons dissolve_select_two_args_arg) ) ))
 
+
+ (define-bounded (vec_map x map_int_to_int)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (map_int_to_int (list-ref-noerr x 0 )) (vec_map (list-tail-noerr x 1 ) map_int_to_int) ) ))
+
+
+ (define-bounded (vec_scalar_add a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (+ a (list-ref-noerr x 0 ) ) (vec_scalar_add a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (vec_scalar_sub a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (- (list-ref-noerr x 0 ) a ) (vec_scalar_sub a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (vec_scalar_mul a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (* a (list-ref-noerr x 0 ) ) (vec_scalar_mul a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (vec_scalar_div a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (quotient-noerr (list-ref-noerr x 0 ) a ) (vec_scalar_div a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (scalar_vec_sub a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (- a (list-ref-noerr x 0 ) ) (scalar_vec_sub a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (scalar_vec_div a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (quotient-noerr a (list-ref-noerr x 0 ) ) (scalar_vec_div a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (matrix_scalar_add a matrix_x)
+(if (< (list-list-length matrix_x ) 1 ) (list-list-empty ) (list-list-prepend (vec_scalar_add a (list-list-ref-noerr matrix_x 0 )) (matrix_scalar_add a (list-list-tail-noerr matrix_x 1 ) ) ) ))
+
+
+ (define-bounded (matrix_scalar_sub a matrix_x)
+(if (< (list-list-length matrix_x ) 1 ) (list-list-empty ) (list-list-prepend (vec_scalar_sub a (list-list-ref-noerr matrix_x 0 )) (matrix_scalar_sub a (list-list-tail-noerr matrix_x 1 ) ) ) ))
+
+
+ (define-bounded (matrix_scalar_mul a matrix_x)
+(if (< (list-list-length matrix_x ) 1 ) (list-list-empty ) (list-list-prepend (vec_scalar_mul a (list-list-ref-noerr matrix_x 0 )) (matrix_scalar_mul a (list-list-tail-noerr matrix_x 1 ) ) ) ))
+
+
+ (define-bounded (matrix_scalar_div a matrix_x)
+(if (< (list-list-length matrix_x ) 1 ) (list-list-empty ) (list-list-prepend (vec_scalar_div a (list-list-ref-noerr matrix_x 0 )) (matrix_scalar_div a (list-list-tail-noerr matrix_x 1 ) ) ) ))
+
+
+ (define-bounded (scalar_matrix_sub a matrix_x)
+(if (< (list-list-length matrix_x ) 1 ) (list-list-empty ) (list-list-prepend (scalar_vec_sub a (list-list-ref-noerr matrix_x 0 )) (scalar_matrix_sub a (list-list-tail-noerr matrix_x 1 )) ) ))
+
+
+ (define-bounded (scalar_matrix_div a matrix_x)
+(if (< (list-list-length matrix_x ) 1 ) (list-list-empty ) (list-list-prepend (scalar_vec_div a (list-list-ref-noerr matrix_x 0 )) (scalar_matrix_div a (list-list-tail-noerr matrix_x 1 )) ) ))
+
+
+ (define-bounded (vec_elemwise_add x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (+ (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_add (list-tail-noerr x 1 ) (list-tail-noerr y 1 )) ) ))
+
+
+ (define-bounded (vec_elemwise_sub x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (- (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_sub (list-tail-noerr x 1 ) (list-tail-noerr y 1 )) ) ))
+
+
+ (define-bounded (vec_elemwise_mul x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (* (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_mul (list-tail-noerr x 1 ) (list-tail-noerr y 1 )) ) ))
+
+
+ (define-bounded (vec_elemwise_div x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (quotient-noerr (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_div (list-tail-noerr x 1 ) (list-tail-noerr y 1 )) ) ))
+
+
+ (define-bounded (matrix_elemwise_add matrix_x matrix_y)
+(if (or (< (list-list-length matrix_x ) 1 ) (! (equal? (list-list-length matrix_x ) (list-list-length matrix_y ) ) ) ) (list-list-empty ) (list-list-prepend (vec_elemwise_add (list-list-ref-noerr matrix_x 0 ) (list-list-ref-noerr matrix_y 0 )) (matrix_elemwise_add (list-list-tail-noerr matrix_x 1 ) (list-list-tail-noerr matrix_y 1 ) ) ) ))
+
+
+ (define-bounded (matrix_elemwise_sub matrix_x matrix_y)
+(if (or (< (list-list-length matrix_x ) 1 ) (! (equal? (list-list-length matrix_x ) (list-list-length matrix_y ) ) ) ) (list-list-empty ) (list-list-prepend (vec_elemwise_sub (list-list-ref-noerr matrix_x 0 ) (list-list-ref-noerr matrix_y 0 )) (matrix_elemwise_sub (list-list-tail-noerr matrix_x 1 ) (list-list-tail-noerr matrix_y 1 ) ) ) ))
+
+
+ (define-bounded (matrix_elemwise_mul matrix_x matrix_y)
+(if (or (< (list-list-length matrix_x ) 1 ) (! (equal? (list-list-length matrix_x ) (list-list-length matrix_y ) ) ) ) (list-list-empty ) (list-list-prepend (vec_elemwise_mul (list-list-ref-noerr matrix_x 0 ) (list-list-ref-noerr matrix_y 0 )) (matrix_elemwise_mul (list-list-tail-noerr matrix_x 1 ) (list-list-tail-noerr matrix_y 1 ) ) ) ))
+
+
+ (define-bounded (matrix_elemwise_div matrix_x matrix_y)
+(if (or (< (list-list-length matrix_x ) 1 ) (! (equal? (list-list-length matrix_x ) (list-list-length matrix_y ) ) ) ) (list-list-empty ) (list-list-prepend (vec_elemwise_div (list-list-ref-noerr matrix_x 0 ) (list-list-ref-noerr matrix_y 0 )) (matrix_elemwise_div (list-list-tail-noerr matrix_x 1 ) (list-list-tail-noerr matrix_y 1 ) ) ) ))
+
 (define-grammar (dissolve_blend_8_inv0_gram active agg.result base col opacity pixel rand_cons rand_val row row_vec)
  [rv (choose (&& (&& (>= row 0 ) (<= row (list-list-length base ) ) ) (equal? agg.result (dissolve_matrix_selection_two_args (v0) (v0) opacity rand_cons dissolve_select_two_args) ) ))]
-[v0 (choose (if (OUTER_LOOP_INDEX_FIRST) (list-list-take-noerr (v1) row ) (list-list-col-slice-noerr (v1) 0 row ) ) (matrix-transpose-noerr (if (OUTER_LOOP_INDEX_FIRST) (list-list-take-noerr (v1) row ) (list-list-col-slice-noerr (v1) 0 row ) ) ))]
+[v0 (choose (list-list-col-slice-noerr (list-list-slice-noerr (v1) (v2) (v2) ) (v2) (v2) ) (matrix-transpose-noerr (list-list-col-slice-noerr (list-list-slice-noerr (v1) (v2) (v2) ) (v2) (v2) ) ))]
 [v1 (choose base active)]
+[v2 (choose 0 (list-list-length base ) row)]
 )
 
 (define-grammar (dissolve_blend_8_inv1_gram active base col opacity pixel rand_cons rand_val row_vec agg.result row)
- [rv (choose (&& (&& (&& (&& (&& (>= row 0 ) (<= row (list-list-length base ) ) ) (>= col 0 ) ) (<= col (length (list-list-ref-noerr base 0 ) ) ) ) (equal? row_vec (dissolve_selection_two_args (if (OUTER_LOOP_INDEX_FIRST) (list-take-noerr (list-list-ref-noerr (v0) row ) col ) (list-list-ref-noerr (matrix-transpose-noerr (list-list-col-slice-with-length-noerr (list-list-take-noerr (v0) col ) row 1 ) ) 0 ) ) (if (OUTER_LOOP_INDEX_FIRST) (list-take-noerr (list-list-ref-noerr (v0) row ) col ) (list-list-ref-noerr (matrix-transpose-noerr (list-list-col-slice-with-length-noerr (list-list-take-noerr (v0) col ) row 1 ) ) 0 ) ) opacity rand_cons dissolve_select_two_args) ) ) (equal? agg.result (dissolve_matrix_selection_two_args (v1) (v1) opacity rand_cons dissolve_select_two_args) ) ))]
-[v0 (choose base active)]
-[v1 (choose (if (OUTER_LOOP_INDEX_FIRST) (list-list-take-noerr (v0) row ) (list-list-col-slice-noerr (v0) 0 row ) ) (matrix-transpose-noerr (if (OUTER_LOOP_INDEX_FIRST) (list-list-take-noerr (v0) row ) (list-list-col-slice-noerr (v0) 0 row ) ) ))]
+ [rv (choose (&& (&& (&& (&& (&& (>= row 0 ) (<= row (list-list-length base ) ) ) (>= col 0 ) ) (<= col (length (list-list-ref-noerr base 0 ) ) ) ) (equal? row_vec (dissolve_selection_two_args (list-list-ref-noerr (v0) (v2) ) (list-list-ref-noerr (v0) (v2) ) opacity rand_cons dissolve_select_two_args) ) ) (equal? agg.result (dissolve_matrix_selection_two_args (v0) (v0) opacity rand_cons dissolve_select_two_args) ) ))]
+[v0 (choose (list-list-col-slice-noerr (list-list-slice-noerr (v1) (v2) (v2) ) (v2) (v2) ) (matrix-transpose-noerr (list-list-col-slice-noerr (list-list-slice-noerr (v1) (v2) (v2) ) (v2) (v2) ) ))]
+[v1 (choose base active)]
+[v2 (choose 0 (list-list-length base ) (length (list-list-ref-noerr base 0 ) ) row col)]
 )
 
 (define-grammar (dissolve_blend_8_ps_gram base active opacity rand_cons dissolve_blend_8_rv)
- [rv (choose (equal? dissolve_blend_8_rv (dissolve_matrix_selection_two_args (v0) (v0) opacity rand_cons dissolve_select_two_args) ))]
+ [rv (choose (equal? dissolve_blend_8_rv (dissolve_matrix_selection_two_args (list-list-col-slice-noerr (list-list-slice-noerr (v0) (v2) (v2) ) (v2) (v2) ) (list-list-col-slice-noerr (list-list-slice-noerr (v0) (v2) (v2) ) (v2) (v2) ) opacity rand_cons dissolve_select_two_args) ))]
 [v0 (choose (v1) (matrix-transpose-noerr (v1) ))]
 [v1 (choose base active)]
+[v2 (choose 0 (list-list-length base ) (length (list-list-ref-noerr base 0 ) ))]
 )
 
 (define-grammar (dissolve_select_two_args_gram int_x int_y opacity rand_cons)
  [rv (choose (v0))]
-[v0 (choose (v1) (v2) (v3) (v4) (v5))]
-[v1 (choose int_x int_y opacity rand_cons)]
-[v2 (choose (+ (v1) (v1) ) (- (v1) (v1) ) (* (v1) (v1) ) (quotient-noerr (v1) (v1) ))]
-[v3 (choose (+ (v2) (v1) ) (- (v2) (v1) ) (* (v2) (v1) ) (quotient-noerr (v2) (v1) ) (- (v1) (v2) ) (quotient-noerr (v1) (v2) ) (+ (v2) (v2) ) (- (v2) (v2) ) (* (v2) (v2) ) (quotient-noerr (v2) (v2) ))]
-[v4 (choose (+ (v3) (v1) ) (- (v3) (v1) ) (* (v3) (v1) ) (quotient-noerr (v3) (v1) ) (- (v1) (v3) ) (quotient-noerr (v1) (v3) ) (+ (v3) (v2) ) (- (v3) (v2) ) (* (v3) (v2) ) (quotient-noerr (v3) (v2) ) (- (v2) (v3) ) (quotient-noerr (v2) (v3) ) (+ (v3) (v3) ) (- (v3) (v3) ) (* (v3) (v3) ) (quotient-noerr (v3) (v3) ))]
-[v5 (choose (+ (v4) (v1) ) (- (v4) (v1) ) (* (v4) (v1) ) (quotient-noerr (v4) (v1) ) (- (v1) (v4) ) (quotient-noerr (v1) (v4) ) (+ (v4) (v2) ) (- (v4) (v2) ) (* (v4) (v2) ) (quotient-noerr (v4) (v2) ) (- (v2) (v4) ) (quotient-noerr (v2) (v4) ) (+ (v4) (v3) ) (- (v4) (v3) ) (* (v4) (v3) ) (quotient-noerr (v4) (v3) ) (- (v3) (v4) ) (quotient-noerr (v3) (v4) ) (+ (v4) (v4) ) (- (v4) (v4) ) (* (v4) (v4) ) (quotient-noerr (v4) (v4) ))]
+[v0 (choose (v1) (+ (v1) (v1) ) (- (v1) (v1) ) (* (v1) (v1) ) (quotient-noerr (v1) (v1) ) (integer-exp-noerr (v1) ) (integer-sqrt-noerr (v1) ))]
+[v1 (choose (v2) (+ (v2) (v2) ) (- (v2) (v2) ) (* (v2) (v2) ) (quotient-noerr (v2) (v2) ) (integer-exp-noerr (v2) ) (integer-sqrt-noerr (v2) ))]
+[v2 (choose (v3) (+ (v3) (v3) ) (- (v3) (v3) ) (* (v3) (v3) ) (quotient-noerr (v3) (v3) ) (integer-exp-noerr (v3) ) (integer-sqrt-noerr (v3) ))]
+[v3 (choose (v4) (+ (v4) (v4) ) (- (v4) (v4) ) (* (v4) (v4) ) (quotient-noerr (v4) (v4) ) (integer-exp-noerr (v4) ) (integer-sqrt-noerr (v4) ))]
+[v4 (choose int_x int_y opacity rand_cons)]
 )
 
-(define-grammar (OUTER_LOOP_INDEX_FIRST_gram )
+(define-grammar (map_int_to_int_gram int_x)
  [rv (choose (v0))]
-[v0 (choose true false)]
+[v0 (choose (integer-exp-noerr int_x ) (integer-sqrt-noerr int_x ))]
 )
 
 (define (dissolve_blend_8_inv0 active agg.result base col opacity pixel rand_cons rand_val row row_vec) (dissolve_blend_8_inv0_gram active agg.result base col opacity pixel rand_cons rand_val row row_vec #:depth 10))
 (define (dissolve_blend_8_inv1 active base col opacity pixel rand_cons rand_val row_vec agg.result row) (dissolve_blend_8_inv1_gram active base col opacity pixel rand_cons rand_val row_vec agg.result row #:depth 10))
 (define (dissolve_blend_8_ps base active opacity rand_cons dissolve_blend_8_rv) (dissolve_blend_8_ps_gram base active opacity rand_cons dissolve_blend_8_rv #:depth 10))
 
-(define (OUTER_LOOP_INDEX_FIRST ) (OUTER_LOOP_INDEX_FIRST_gram  #:depth 10))
 (define (dissolve_select_two_args int_x int_y opacity rand_cons) (dissolve_select_two_args_gram int_x int_y opacity rand_cons #:depth 10))
+(define (map_int_to_int int_x) (map_int_to_int_gram int_x #:depth 10))
 
 (define-symbolic active_BOUNDEDSET-len integer?)
 (define-symbolic active_BOUNDEDSET-0 integer?)
@@ -76,7 +170,9 @@
 (define-symbolic dissolve_blend_8_rv_BOUNDEDSET-len integer?)
 (define-symbolic dissolve_blend_8_rv_BOUNDEDSET-0 integer?)
 (define-symbolic dissolve_blend_8_rv_BOUNDEDSET-1 integer?)
-(define dissolve_blend_8_rv (take (list dissolve_blend_8_rv_BOUNDEDSET-0 dissolve_blend_8_rv_BOUNDEDSET-1) dissolve_blend_8_rv_BOUNDEDSET-len))
+(define-symbolic dissolve_blend_8_rv_BOUNDEDSET-2 integer?)
+(define-symbolic dissolve_blend_8_rv_BOUNDEDSET-3 integer?)
+(define dissolve_blend_8_rv (take (list (list dissolve_blend_8_rv_BOUNDEDSET-0 dissolve_blend_8_rv_BOUNDEDSET-1) (list dissolve_blend_8_rv_BOUNDEDSET-2 dissolve_blend_8_rv_BOUNDEDSET-3)) dissolve_blend_8_rv_BOUNDEDSET-len))
 (define-symbolic int_x integer?)
 (define-symbolic int_y integer?)
 (define-symbolic opacity integer?)
@@ -95,7 +191,7 @@
 
     (define sol0
         (synthesize
-            #:forall (list active_BOUNDEDSET-len active_BOUNDEDSET-0 active_BOUNDEDSET-1 active_BOUNDEDSET-2 active_BOUNDEDSET-3 agg.result_BOUNDEDSET-len agg.result_BOUNDEDSET-0 agg.result_BOUNDEDSET-1 agg.result_BOUNDEDSET-2 agg.result_BOUNDEDSET-3 base_BOUNDEDSET-len base_BOUNDEDSET-0 base_BOUNDEDSET-1 base_BOUNDEDSET-2 base_BOUNDEDSET-3 col dissolve_blend_8_rv_BOUNDEDSET-len dissolve_blend_8_rv_BOUNDEDSET-0 dissolve_blend_8_rv_BOUNDEDSET-1 int_x int_y opacity pixel rand_cons rand_val row row_vec_BOUNDEDSET-len row_vec_BOUNDEDSET-0 row_vec_BOUNDEDSET-1)
+            #:forall (list active_BOUNDEDSET-len active_BOUNDEDSET-0 active_BOUNDEDSET-1 active_BOUNDEDSET-2 active_BOUNDEDSET-3 agg.result_BOUNDEDSET-len agg.result_BOUNDEDSET-0 agg.result_BOUNDEDSET-1 agg.result_BOUNDEDSET-2 agg.result_BOUNDEDSET-3 base_BOUNDEDSET-len base_BOUNDEDSET-0 base_BOUNDEDSET-1 base_BOUNDEDSET-2 base_BOUNDEDSET-3 col dissolve_blend_8_rv_BOUNDEDSET-len dissolve_blend_8_rv_BOUNDEDSET-0 dissolve_blend_8_rv_BOUNDEDSET-1 dissolve_blend_8_rv_BOUNDEDSET-2 dissolve_blend_8_rv_BOUNDEDSET-3 int_x int_y opacity pixel rand_cons rand_val row row_vec_BOUNDEDSET-len row_vec_BOUNDEDSET-0 row_vec_BOUNDEDSET-1)
             #:guarantee (assertions)
         )
     )
