1329221df986cf02a611e9b96ae393c290c160c1
Jie Qiu <jieqiu0630@gmail.com>
2024-03-05 13:08:46 -0500

    Add dsl code for matrix operations

--- tenspiler/tenspiler_common.py
+++ tenspiler/tenspiler_common.py
@@ -2041,3 +2041,82 @@ def get_fn_and_rv(fn_name: str, body: Any, fn_args: List[Any]) -> Any:
     fn_synth = synth(fn_name, body, *fn_args)
     rv = call(fn_name, body.type, *fn_args)
     return decl, fn_synth, rv
+
+
+# list access function decls
+# Define some arguments to be used
+lst = mlList(Int, "lst")
+matrix = Matrix(Int, "matrix")
+start = Int("start")
+end = Int("end")
+lst_length = Int("lst_length")
+
+# list_slice
+list_slice_fn_decl = fn_decl(
+    "list_slice", mlList[Int], lst[:end][:start:], lst, start, end
+)
+
+# list_slice_with_length
+list_slice_with_length_fn_decl = fn_decl(
+    "list_slice_with_length",
+    mlList[Int],
+    lst[start : start + lst_length],
+    lst,
+    start,
+    lst_length,
+)
+
+# list_list_slice
+list_list_slice_fn_decl = fn_decl(
+    "list_list_slice", Matrix[Int], matrix[:end][:start:], matrix, start, end
+)
+
+# list_slice_with_length
+list_list_slice_with_length_fn_decl = fn_decl(
+    "list_list_slice_with_length",
+    Matrix[Int],
+    matrix[start : start + lst_length],
+    matrix,
+    start,
+    lst_length,
+)
+
+# list_list_col_slice
+list_list_col_slice_body = ite(
+    matrix.len() < 1,
+    Matrix.empty(Int),
+    matrix[1:].col_slice(start, end).prepend(matrix[0][start:end]),
+)
+list_list_col_slice_fn_decl = fn_decl_recursive(
+    "list_list_col_slice", Matrix[Int], list_list_col_slice_body, matrix, start, end
+)
+# list_list_col_slice_with_length
+list_list_col_slice_with_length_fn_decl = fn_decl_recursive(
+    "list_list_col_slice_with_length",
+    Matrix[Int],
+    matrix.col_slice(start, start + lst_length),
+    matrix,
+    start,
+    lst_length,
+)
+# matrix transpose
+firsts_body = ite(
+    matrix.len() < 1,
+    mlList.empty(Int),
+    call("firsts", mlList[Int], matrix[1:]).prepend(matrix[0][0]),
+)
+firsts_fn_decl = fn_decl_recursive("firsts", mlList[Int], firsts_body, matrix)
+rests_body = ite(
+    matrix.len() < 1, Matrix.empty(Int), matrix.col_slice(1, matrix[0].len())
+)
+rests_fn_decl = fn_decl_recursive("rests", Matrix[Int], rests_body, matrix)
+matrix_transpose_body = ite(
+    matrix.len() < 1,
+    Matrix.empty(Int),
+    call("rests", Matrix[Int], matrix)
+    .transpose()
+    .prepend(call("firsts", mlList[Int], matrix)),
+)
+matrix_transpose_fn_decl = fn_decl_recursive(
+    "matrix_transpose", Matrix[Int], matrix_transpose_body, matrix
+)
