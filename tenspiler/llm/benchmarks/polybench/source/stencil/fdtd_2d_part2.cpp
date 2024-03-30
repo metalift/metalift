c0d84eae636401fa202eded57ea3be66091f2073
Jie Qiu <jieqiu0630@gmail.com>
2024-03-30 17:07:42 -0400

    Add fdtd

--- tenspiler/llm/benchmarks/polybench/source/stencil/fdtd_2d_part2.cpp
+++ tenspiler/llm/benchmarks/polybench/source/stencil/fdtd_2d_part2.cpp
@@ -1,17 +1,17 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> fdtd_2d_part1(
+vector<vector<int>> fdtd_2d_part2(
     int nx,
     int ny,
-    vector<vector<int>> ey,
+    vector<vector<int>> ex,
     vector<vector<int>> hz
 ) {
     vector<vector<int>> out;
-    for (int i = 1; i < nx; i++) {
+    for (int i = 0; i < nx; i++) {
         vector<int> row_vec;
-        for (int j = 0; j < ny; j++) {
-            int curr = ey[i][j] - 5 * (hz[i][j] - hz[i - 1][j]);
+        for (int j = 1; j < ny; j++) {
+            int curr = ex[i][j] - 5 * (hz[i][j] - hz[i][j - 1]);
             row_vec.push_back(curr);
         }
         out.push_back(row_vec);
