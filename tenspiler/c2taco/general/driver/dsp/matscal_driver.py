af589a327a5fb367e761b3b7d4865fa4d7a39092
Jie Qiu <jieqiu0630@gmail.com>
2024-04-12 13:26:17 -0400

    Add matscal

---
+++ tenspiler/c2taco/general/driver/dsp/matscal_driver.py
@@ -0,0 +1,146 @@
+import argparse
+import time
+from typing import List, Union
+
+from metalift.frontend.llvm import Driver, InvGrammar
+from metalift.ir import Bool, FnDecl, FnDeclRecursive, Int, Matrix, Object, choose
+from metalift.vc_util import and_objects
+from tenspiler.tenspiler_common import (
+    get_int_expr_eq_or_below_depth,
+    get_map_int_to_int_synth,
+    get_matrix_or_vec_expr_eq_or_below_depth,
+    matrix_matrix_to_matrix_target_lang,
+    matrix_vec_mul,
+    reduce_sum,
+    scalar_matrix_to_matrix_target_lang,
+    scalar_vec_to_vec_target_lang,
+    vec_to_vec_target_lang,
+    vec_vec_to_vec_target_lang,
+)
+
+
+def matscal_target_lang() -> List[Union[FnDecl, FnDeclRecursive]]:
+    return [
+        matrix_vec_mul,
+        reduce_sum,
+        *vec_to_vec_target_lang,
+        *scalar_vec_to_vec_target_lang,
+        *scalar_matrix_to_matrix_target_lang,
+        *vec_vec_to_vec_target_lang,
+        *matrix_matrix_to_matrix_target_lang,
+    ]
+
+
+def matscal_inv0_grammar(
+    writes: List[Object], reads: List[Object], in_scope: List[Object]
+) -> Bool:
+    mat, val, m, n = reads
+    out, i, j, _, _ = writes
+    lower_bound, upper_bound = Int(0), m
+    int_var = choose(Int(0), m, n, i, val).maybe_relaxed(parser_args.relaxed)
+    slice_index = get_int_expr_eq_or_below_depth(int_var, parser_args.depth)
+    matrix = mat[slice_index:slice_index].col_slice(slice_index, slice_index)
+    matrix = choose(matrix, matrix.transpose())
+    return and_objects(
+        i >= lower_bound.maybe_relaxed(parser_args.relaxed),
+        i <= upper_bound.maybe_relaxed(parser_args.relaxed),
+        out
+        == get_matrix_or_vec_expr_eq_or_below_depth(
+            matrix_or_vec_var=matrix,
+            int_var=int_var,
+            depth=parser_args.depth,
+        ),
+    )
+
+
+def matscal_inv1_grammar(
+    writes: List[Object], reads: List[Object], in_scope: List[Object]
+) -> Bool:
+    mat, val, m, n = reads
+    out, i = in_scope
+    j, _, row_vec = writes
+    int_var = choose(Int(0), m, n, i, j, val).maybe_relaxed(parser_args.relaxed)
+    slice_index = get_int_expr_eq_or_below_depth(int_var, parser_args.depth)
+    matrix = mat[slice_index:slice_index].col_slice(slice_index, slice_index)
+    matrix = choose(matrix, matrix.transpose())
+    vec = matrix[slice_index]
+
+    outer_loop_lower_bound, outer_loop_upper_bound = Int(0), m
+    inner_loop_lower_bound, inner_loop_upper_bound = Int(0), n
+    return and_objects(
+        i >= outer_loop_lower_bound.maybe_relaxed(parser_args.relaxed),
+        i < outer_loop_upper_bound.maybe_relaxed(parser_args.relaxed),
+        j >= inner_loop_lower_bound.maybe_relaxed(parser_args.relaxed),
+        j <= inner_loop_upper_bound.maybe_relaxed(parser_args.relaxed),
+        row_vec
+        == get_matrix_or_vec_expr_eq_or_below_depth(
+            matrix_or_vec_var=vec,
+            int_var=int_var,
+            depth=parser_args.depth,
+            additional_matrix=matrix,
+        ),
+        out
+        == get_matrix_or_vec_expr_eq_or_below_depth(
+            matrix_or_vec_var=matrix, int_var=int_var, depth=parser_args.depth
+        ),
+    )
+
+
+def matscal_ps_grammar(
+    writes: List[Object], reads: List[Object], in_scope: List[Object]
+) -> Bool:
+    mat, val, m, n = reads
+    out = writes[0]
+    int_var = choose(Int(0), m, n, val).maybe_relaxed(parser_args.relaxed)
+    slice_index = get_int_expr_eq_or_below_depth(int_var, parser_args.depth)
+    matrix = mat[slice_index:slice_index].col_slice(slice_index, slice_index)
+    matrix = choose(matrix, matrix.transpose())
+    return out == get_matrix_or_vec_expr_eq_or_below_depth(
+        matrix_or_vec_var=matrix, int_var=int_var, depth=parser_args.depth
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--depth", type=int)
+    parser.add_argument("--relaxed", action="store_true")
+    parser_args = parser.parse_args()
+
+    driver = Driver()
+    matscal = driver.analyze(
+        llvm_filepath="tenspiler/c2taco/cpp/for_synthesis/dsp/matscal.ll",
+        loops_filepath="tenspiler/c2taco/cpp/for_synthesis/dsp/matscal.loops",
+        fn_name="matscal",
+        target_lang_fn=matscal_target_lang,
+        inv_grammars={
+            "matscal_inv0": InvGrammar(matscal_inv0_grammar, []),
+            "matscal_inv1": InvGrammar(matscal_inv1_grammar, ["i", "agg.result"]),
+        },
+        ps_grammar=matscal_ps_grammar,
+    )
+
+    mat = Matrix(Int, "mat")
+    val = Int("val")
+    m = Int("m")
+    n = Int("n")
+    driver.add_var_objects([mat, val, m, n])
+
+    # Add preconditions
+    driver.add_precondition(m >= 1)
+    driver.add_precondition(n >= 1)
+    driver.add_precondition(mat.len() >= m)
+    driver.add_precondition(mat[0].len() >= n)
+
+    map_int_to_int_synth = get_map_int_to_int_synth()
+    driver.fns_synths = [
+        map_int_to_int_synth,
+    ]
+
+    matscal(mat, val, m, n)
+
+    start_time = time.time()
+    relaxed_suffix = "_relaxed" if parser_args.relaxed else ""
+    depth_suffix = f"_depth{parser_args.depth}"
+    driver.synthesize(filename=f"matscal{depth_suffix}{relaxed_suffix}", no_verify=True)
+    end_time = time.time()
+    print(f"Synthesis took {end_time - start_time} seconds")
---
+++ tenspiler/c2taco/general/racket/listbound2/dsp/matscal/matscal_depth0.rkt
@@ -0,0 +1,176 @@
+#lang rosette
+(require "./bounded.rkt")
+(require "./utils.rkt")
+(require rosette/lib/angelic rosette/lib/match rosette/lib/synthax)
+(require rosette/solver/smt/bitwuzla)
+(current-solver (bitwuzla #:path "/Users/sahilbhatia/Documents/bitwuzla/build/src/main/bitwuzla" #:options (hash ':seed 0)))
+
+
+ (define-bounded (matrix_vec_mul matrix_x x)
+(if (or (or (< (matrix-length matrix_x ) 1 ) (< (length (matrix-ref-noerr matrix_x 0 ) ) 1 ) ) (! (equal? (length (matrix-ref-noerr matrix_x 0 ) ) (length x ) ) ) ) (list-empty ) (list-prepend (reduce_sum (vec_elemwise_mul (matrix-ref-noerr matrix_x 0 ) x )) (matrix_vec_mul (matrix-tail-noerr matrix_x 1 ) x ) ) ))
+
+
+ (define-bounded (reduce_sum x)
+(if (< (length x ) 1 ) 0 (+ (list-ref-noerr x 0 ) (reduce_sum (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (vec_map x map_int_to_int)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (map_int_to_int (list-ref-noerr x 0 )) (vec_map (list-tail-noerr x 1 ) map_int_to_int ) ) ))
+
+
+ (define-bounded (vec_scalar_add a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (+ a (list-ref-noerr x 0 ) ) (vec_scalar_add a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_sub a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (- (list-ref-noerr x 0 ) a ) (vec_scalar_sub a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_mul a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (* a (list-ref-noerr x 0 ) ) (vec_scalar_mul a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_div a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (quotient-noerr (list-ref-noerr x 0 ) a ) (vec_scalar_div a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (scalar_vec_sub a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (- a (list-ref-noerr x 0 ) ) (scalar_vec_sub a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (scalar_vec_div a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (quotient-noerr a (list-ref-noerr x 0 ) ) (scalar_vec_div a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (matrix_scalar_add a matrix_x)
+(if (< (matrix-length matrix_x ) 1 ) (matrix-empty ) (matrix-prepend (vec_scalar_add a (matrix-ref-noerr matrix_x 0 ) ) (matrix_scalar_add a (matrix-tail-noerr matrix_x 1 ) ) ) ))
+
+
+ (define-bounded (matrix_scalar_sub a matrix_x)
+(if (< (matrix-length matrix_x ) 1 ) (matrix-empty ) (matrix-prepend (vec_scalar_sub a (matrix-ref-noerr matrix_x 0 ) ) (matrix_scalar_sub a (matrix-tail-noerr matrix_x 1 ) ) ) ))
+
+
+ (define-bounded (matrix_scalar_mul a matrix_x)
+(if (< (matrix-length matrix_x ) 1 ) (matrix-empty ) (matrix-prepend (vec_scalar_mul a (matrix-ref-noerr matrix_x 0 ) ) (matrix_scalar_mul a (matrix-tail-noerr matrix_x 1 ) ) ) ))
+
+
+ (define-bounded (matrix_scalar_div a matrix_x)
+(if (< (matrix-length matrix_x ) 1 ) (matrix-empty ) (matrix-prepend (vec_scalar_div a (matrix-ref-noerr matrix_x 0 ) ) (matrix_scalar_div a (matrix-tail-noerr matrix_x 1 ) ) ) ))
+
+
+ (define-bounded (scalar_matrix_sub a matrix_x)
+(if (< (matrix-length matrix_x ) 1 ) (matrix-empty ) (matrix-prepend (scalar_vec_sub a (matrix-ref-noerr matrix_x 0 )) (scalar_matrix_sub a (matrix-tail-noerr matrix_x 1 )) ) ))
+
+
+ (define-bounded (scalar_matrix_div a matrix_x)
+(if (< (matrix-length matrix_x ) 1 ) (matrix-empty ) (matrix-prepend (scalar_vec_div a (matrix-ref-noerr matrix_x 0 )) (scalar_matrix_div a (matrix-tail-noerr matrix_x 1 )) ) ))
+
+
+ (define-bounded (vec_elemwise_add x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (+ (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_add (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_sub x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (- (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_sub (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_mul x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (* (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_mul (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_div x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (quotient-noerr (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_div (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (matrix_elemwise_add matrix_x matrix_y)
+(if (or (< (matrix-length matrix_x ) 1 ) (! (equal? (matrix-length matrix_x ) (matrix-length matrix_y ) ) ) ) (matrix-empty ) (matrix-prepend (vec_elemwise_add (matrix-ref-noerr matrix_x 0 ) (matrix-ref-noerr matrix_y 0 ) ) (matrix_elemwise_add (matrix-tail-noerr matrix_x 1 ) (matrix-tail-noerr matrix_y 1 ) ) ) ))
+
+
+ (define-bounded (matrix_elemwise_sub matrix_x matrix_y)
+(if (or (< (matrix-length matrix_x ) 1 ) (! (equal? (matrix-length matrix_x ) (matrix-length matrix_y ) ) ) ) (matrix-empty ) (matrix-prepend (vec_elemwise_sub (matrix-ref-noerr matrix_x 0 ) (matrix-ref-noerr matrix_y 0 ) ) (matrix_elemwise_sub (matrix-tail-noerr matrix_x 1 ) (matrix-tail-noerr matrix_y 1 ) ) ) ))
+
+
+ (define-bounded (matrix_elemwise_mul matrix_x matrix_y)
+(if (or (< (matrix-length matrix_x ) 1 ) (! (equal? (matrix-length matrix_x ) (matrix-length matrix_y ) ) ) ) (matrix-empty ) (matrix-prepend (vec_elemwise_mul (matrix-ref-noerr matrix_x 0 ) (matrix-ref-noerr matrix_y 0 ) ) (matrix_elemwise_mul (matrix-tail-noerr matrix_x 1 ) (matrix-tail-noerr matrix_y 1 ) ) ) ))
+
+
+ (define-bounded (matrix_elemwise_div matrix_x matrix_y)
+(if (or (< (matrix-length matrix_x ) 1 ) (! (equal? (matrix-length matrix_x ) (matrix-length matrix_y ) ) ) ) (matrix-empty ) (matrix-prepend (vec_elemwise_div (matrix-ref-noerr matrix_x 0 ) (matrix-ref-noerr matrix_y 0 ) ) (matrix_elemwise_div (matrix-tail-noerr matrix_x 1 ) (matrix-tail-noerr matrix_y 1 ) ) ) ))
+
+(define-grammar (matscal_inv0_gram agg.result i j m mat n ref.tmp row_vec val)
+ [rv (choose (&& (&& (>= i 0 ) (<= i m ) ) (equal? agg.result (v0) ) ))]
+[v0 (choose (v1))]
+[v1 (choose (matrix-col-slice-noerr (matrix-row-slice-noerr mat (v2) (v2) ) (v2) (v2) ) (matrix-transpose-noerr (matrix-col-slice-noerr (matrix-row-slice-noerr mat (v2) (v2) ) (v2) (v2) ) ))]
+[v2 (choose (v3))]
+[v3 (choose 0 m n i val)]
+)
+
+(define-grammar (matscal_inv1_gram j m mat n ref.tmp row_vec val agg.result i)
+ [rv (choose (&& (&& (&& (&& (&& (>= i 0 ) (< i m ) ) (>= j 0 ) ) (<= j n ) ) (equal? row_vec (v0) ) ) (equal? agg.result (v4) ) ))]
+[v0 (choose (matrix-ref-noerr (v1) (v2) ))]
+[v1 (choose (matrix-col-slice-noerr (matrix-row-slice-noerr mat (v2) (v2) ) (v2) (v2) ) (matrix-transpose-noerr (matrix-col-slice-noerr (matrix-row-slice-noerr mat (v2) (v2) ) (v2) (v2) ) ))]
+[v2 (choose (v3))]
+[v3 (choose 0 m n i j val)]
+[v4 (choose (v1))]
+)
+
+(define-grammar (matscal_ps_gram mat val m n matscal_rv)
+ [rv (choose (equal? matscal_rv (v0) ))]
+[v0 (choose (v1))]
+[v1 (choose (matrix-col-slice-noerr (matrix-row-slice-noerr mat (v2) (v2) ) (v2) (v2) ) (matrix-transpose-noerr (matrix-col-slice-noerr (matrix-row-slice-noerr mat (v2) (v2) ) (v2) (v2) ) ))]
+[v2 (choose (v3))]
+[v3 (choose 0 m n val)]
+)
+
+(define-grammar (map_int_to_int_gram int_x)
+ [rv (choose (v0))]
+[v0 (choose (integer-exp-noerr int_x ) (integer-sqrt-noerr int_x ))]
+)
+
+(define (matscal_inv0 agg.result i j m mat n ref.tmp row_vec val) (matscal_inv0_gram agg.result i j m mat n ref.tmp row_vec val #:depth 10))
+(define (matscal_inv1 j m mat n ref.tmp row_vec val agg.result i) (matscal_inv1_gram j m mat n ref.tmp row_vec val agg.result i #:depth 10))
+(define (matscal_ps mat val m n matscal_rv) (matscal_ps_gram mat val m n matscal_rv #:depth 10))
+
+(define (map_int_to_int int_x) (map_int_to_int_gram int_x #:depth 10))
+
+(define-symbolic agg.result_BOUNDEDSET-len integer?)
+(define-symbolic agg.result_BOUNDEDSET-0 integer?)
+(define-symbolic agg.result_BOUNDEDSET-1 integer?)
+(define-symbolic agg.result_BOUNDEDSET-2 integer?)
+(define-symbolic agg.result_BOUNDEDSET-3 integer?)
+(define agg.result (take (list (list agg.result_BOUNDEDSET-0 agg.result_BOUNDEDSET-1) (list agg.result_BOUNDEDSET-2 agg.result_BOUNDEDSET-3)) agg.result_BOUNDEDSET-len))
+(define-symbolic i integer?)
+(define-symbolic j integer?)
+(define-symbolic m integer?)
+(define-symbolic mat_BOUNDEDSET-len integer?)
+(define-symbolic mat_BOUNDEDSET-0 integer?)
+(define-symbolic mat_BOUNDEDSET-1 integer?)
+(define-symbolic mat_BOUNDEDSET-2 integer?)
+(define-symbolic mat_BOUNDEDSET-3 integer?)
+(define mat (take (list (list mat_BOUNDEDSET-0 mat_BOUNDEDSET-1) (list mat_BOUNDEDSET-2 mat_BOUNDEDSET-3)) mat_BOUNDEDSET-len))
+(define-symbolic matscal_rv_BOUNDEDSET-len integer?)
+(define-symbolic matscal_rv_BOUNDEDSET-0 integer?)
+(define-symbolic matscal_rv_BOUNDEDSET-1 integer?)
+(define-symbolic matscal_rv_BOUNDEDSET-2 integer?)
+(define-symbolic matscal_rv_BOUNDEDSET-3 integer?)
+(define matscal_rv (take (list (list matscal_rv_BOUNDEDSET-0 matscal_rv_BOUNDEDSET-1) (list matscal_rv_BOUNDEDSET-2 matscal_rv_BOUNDEDSET-3)) matscal_rv_BOUNDEDSET-len))
+(define-symbolic n integer?)
+(define-symbolic ref.tmp integer?)
+(define-symbolic row_vec_BOUNDEDSET-len integer?)
+(define-symbolic row_vec_BOUNDEDSET-0 integer?)
+(define-symbolic row_vec_BOUNDEDSET-1 integer?)
+(define row_vec (take (list row_vec_BOUNDEDSET-0 row_vec_BOUNDEDSET-1) row_vec_BOUNDEDSET-len))
+(define-symbolic val integer?)
+(current-bitwidth 6)
+(define (assertions)
+ (assert (&& (&& (&& (&& (=> (&& (&& (&& (>= m 1 ) (>= n 1 ) ) (>= (matrix-length mat ) m ) ) (>= (length (matrix-ref-noerr mat 0 ) ) n ) ) (matscal_inv0 (matrix-empty ) 0 0 m mat n 0 (list-empty ) val) ) (=> (&& (&& (&& (&& (&& (< i m ) (>= m 1 ) ) (>= n 1 ) ) (>= (matrix-length mat ) m ) ) (>= (length (matrix-ref-noerr mat 0 ) ) n ) ) (matscal_inv0 agg.result i j m mat n ref.tmp row_vec val) ) (matscal_inv1 0 m mat n ref.tmp (list-empty ) val agg.result i) ) ) (=> (&& (&& (&& (&& (&& (&& (&& (< j n ) (< i m ) ) (>= m 1 ) ) (>= n 1 ) ) (>= (matrix-length mat ) m ) ) (>= (length (matrix-ref-noerr mat 0 ) ) n ) ) (matscal_inv0 agg.result i j m mat n ref.tmp row_vec val) ) (matscal_inv1 j m mat n ref.tmp row_vec val agg.result i) ) (matscal_inv1 (+ j 1 ) m mat n (* (list-ref-noerr (matrix-ref-noerr mat i ) j ) val ) (list-append row_vec (* (list-ref-noerr (matrix-ref-noerr mat i ) j ) val ) ) val agg.result i) ) ) (=> (&& (&& (&& (&& (&& (&& (&& (! (< j n ) ) (< i m ) ) (>= m 1 ) ) (>= n 1 ) ) (>= (matrix-length mat ) m ) ) (>= (length (matrix-ref-noerr mat 0 ) ) n ) ) (matscal_inv0 agg.result i j m mat n ref.tmp row_vec val) ) (matscal_inv1 j m mat n ref.tmp row_vec val agg.result i) ) (matscal_inv0 (matrix-append agg.result row_vec ) (+ i 1 ) j m mat n ref.tmp row_vec val) ) ) (=> (or (&& (&& (&& (&& (&& (! (< i m ) ) (>= m 1 ) ) (>= n 1 ) ) (>= (matrix-length mat ) m ) ) (>= (length (matrix-ref-noerr mat 0 ) ) n ) ) (matscal_inv0 agg.result i j m mat n ref.tmp row_vec val) ) (&& (&& (&& (&& (&& (&& (! true ) (! (< i m ) ) ) (>= m 1 ) ) (>= n 1 ) ) (>= (matrix-length mat ) m ) ) (>= (length (matrix-ref-noerr mat 0 ) ) n ) ) (matscal_inv0 agg.result i j m mat n ref.tmp row_vec val) ) ) (matscal_ps mat val m n agg.result) ) )))
+
+
+    (define sol0
+        (synthesize
+            #:forall (list agg.result_BOUNDEDSET-len agg.result_BOUNDEDSET-0 agg.result_BOUNDEDSET-1 agg.result_BOUNDEDSET-2 agg.result_BOUNDEDSET-3 i j m mat_BOUNDEDSET-len mat_BOUNDEDSET-0 mat_BOUNDEDSET-1 mat_BOUNDEDSET-2 mat_BOUNDEDSET-3 matscal_rv_BOUNDEDSET-len matscal_rv_BOUNDEDSET-0 matscal_rv_BOUNDEDSET-1 matscal_rv_BOUNDEDSET-2 matscal_rv_BOUNDEDSET-3 n ref.tmp row_vec_BOUNDEDSET-len row_vec_BOUNDEDSET-0 row_vec_BOUNDEDSET-1 val)
+            #:guarantee (assertions)
+        )
+    )
+    (sat? sol0)
+    (print-forms sol0)
