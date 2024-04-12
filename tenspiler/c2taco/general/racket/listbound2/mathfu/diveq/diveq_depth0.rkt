89434569f991d78288d30b8886c2fd1ebccff01f
Jie Qiu <jieqiu0630@gmail.com>
2024-04-12 15:11:00 -0400

    Add diveq_sca and diveq

---
+++ tenspiler/c2taco/general/racket/listbound2/mathfu/diveq/diveq_depth0.rkt
@@ -0,0 +1,121 @@
+#lang rosette
+(require "./bounded.rkt")
+(require "./utils.rkt")
+(require rosette/lib/angelic rosette/lib/match rosette/lib/synthax)
+(require rosette/solver/smt/bitwuzla)
+(current-solver (bitwuzla #:path "/Users/sahilbhatia/Documents/bitwuzla/build/src/main/bitwuzla" #:options (hash ':seed 0)))
+
+
+ (define-bounded (vec_elemwise_add x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (+ (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_add (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_sub x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (- (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_sub (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_mul x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (* (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_mul (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_div x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (quotient-noerr (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_div (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_add a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (+ a (list-ref-noerr x 0 ) ) (vec_scalar_add a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_sub a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (- (list-ref-noerr x 0 ) a ) (vec_scalar_sub a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_mul a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (* a (list-ref-noerr x 0 ) ) (vec_scalar_mul a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_div a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (quotient-noerr (list-ref-noerr x 0 ) a ) (vec_scalar_div a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (scalar_vec_sub a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (- a (list-ref-noerr x 0 ) ) (scalar_vec_sub a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (scalar_vec_div a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (quotient-noerr a (list-ref-noerr x 0 ) ) (scalar_vec_div a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (vec_map x map_int_to_int)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (map_int_to_int (list-ref-noerr x 0 )) (vec_map (list-tail-noerr x 1 ) map_int_to_int ) ) ))
+
+
+ (define-bounded (reduce_max x)
+(if (<= (length x ) 1 ) (list-ref-noerr x 0 ) (if (> (list-ref-noerr x 0 ) (reduce_max (list-tail-noerr x 1 )) ) (list-ref-noerr x 0 ) (reduce_max (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (reduce_sum x)
+(if (< (length x ) 1 ) 0 (+ (list-ref-noerr x 0 ) (reduce_sum (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (reduce_mul x)
+(if (< (length x ) 1 ) 1 (* (list-ref-noerr x 0 ) (reduce_mul (list-tail-noerr x 1 )) ) ))
+
+(define-grammar (diveq_inv0_gram a agg.result b i n ref.tmp)
+ [rv (choose (&& (&& (>= i 0 ) (<= i n ) ) (equal? agg.result (v0) ) ))]
+[v0 (choose (vec-slice-noerr (v1) (v2) (v2) ))]
+[v1 (choose a b)]
+[v2 (choose (v3))]
+[v3 (choose 0 n i)]
+)
+
+(define-grammar (diveq_ps_gram a b n diveq_rv)
+ [rv (choose (equal? diveq_rv (v0) ))]
+[v0 (choose (vec-slice-noerr (v1) (v2) (v2) ))]
+[v1 (choose a b)]
+[v2 (choose (v3))]
+[v3 (choose 0 n)]
+)
+
+(define-grammar (map_int_to_int_gram int_x)
+ [rv (choose (v0))]
+[v0 (choose (integer-exp-noerr int_x ) (integer-sqrt-noerr int_x ))]
+)
+
+(define (diveq_inv0 a agg.result b i n ref.tmp) (diveq_inv0_gram a agg.result b i n ref.tmp #:depth 10))
+(define (diveq_ps a b n diveq_rv) (diveq_ps_gram a b n diveq_rv #:depth 10))
+
+(define (map_int_to_int int_x) (map_int_to_int_gram int_x #:depth 10))
+
+(define-symbolic a_BOUNDEDSET-len integer?)
+(define-symbolic a_BOUNDEDSET-0 integer?)
+(define-symbolic a_BOUNDEDSET-1 integer?)
+(define a (take (list a_BOUNDEDSET-0 a_BOUNDEDSET-1) a_BOUNDEDSET-len))
+(define-symbolic agg.result_BOUNDEDSET-len integer?)
+(define-symbolic agg.result_BOUNDEDSET-0 integer?)
+(define-symbolic agg.result_BOUNDEDSET-1 integer?)
+(define agg.result (take (list agg.result_BOUNDEDSET-0 agg.result_BOUNDEDSET-1) agg.result_BOUNDEDSET-len))
+(define-symbolic b_BOUNDEDSET-len integer?)
+(define-symbolic b_BOUNDEDSET-0 integer?)
+(define-symbolic b_BOUNDEDSET-1 integer?)
+(define b (take (list b_BOUNDEDSET-0 b_BOUNDEDSET-1) b_BOUNDEDSET-len))
+(define-symbolic diveq_rv_BOUNDEDSET-len integer?)
+(define-symbolic diveq_rv_BOUNDEDSET-0 integer?)
+(define-symbolic diveq_rv_BOUNDEDSET-1 integer?)
+(define diveq_rv (take (list diveq_rv_BOUNDEDSET-0 diveq_rv_BOUNDEDSET-1) diveq_rv_BOUNDEDSET-len))
+(define-symbolic i integer?)
+(define-symbolic n integer?)
+(define-symbolic ref.tmp integer?)
+(current-bitwidth 6)
+(define (assertions)
+ (assert (&& (&& (=> (&& (&& (>= n 1 ) (>= (length a ) n ) ) (>= (length b ) n ) ) (diveq_inv0 a (list-empty ) b 0 n 0) ) (=> (&& (&& (&& (&& (< i n ) (>= n 1 ) ) (>= (length a ) n ) ) (>= (length b ) n ) ) (diveq_inv0 a agg.result b i n ref.tmp) ) (diveq_inv0 a (list-append agg.result (quotient-noerr (list-ref-noerr a i ) (list-ref-noerr b i ) ) ) b (+ i 1 ) n (quotient-noerr (list-ref-noerr a i ) (list-ref-noerr b i ) )) ) ) (=> (or (&& (&& (&& (&& (! (< i n ) ) (>= n 1 ) ) (>= (length a ) n ) ) (>= (length b ) n ) ) (diveq_inv0 a agg.result b i n ref.tmp) ) (&& (&& (&& (&& (&& (! true ) (! (< i n ) ) ) (>= n 1 ) ) (>= (length a ) n ) ) (>= (length b ) n ) ) (diveq_inv0 a agg.result b i n ref.tmp) ) ) (diveq_ps a b n agg.result) ) )))
+
+
+    (define sol0
+        (synthesize
+            #:forall (list a_BOUNDEDSET-len a_BOUNDEDSET-0 a_BOUNDEDSET-1 agg.result_BOUNDEDSET-len agg.result_BOUNDEDSET-0 agg.result_BOUNDEDSET-1 b_BOUNDEDSET-len b_BOUNDEDSET-0 b_BOUNDEDSET-1 diveq_rv_BOUNDEDSET-len diveq_rv_BOUNDEDSET-0 diveq_rv_BOUNDEDSET-1 i n ref.tmp)
+            #:guarantee (assertions)
+        )
+    )
+    (sat? sol0)
+    (print-forms sol0)
---
+++ tenspiler/c2taco/general/racket/listbound2/mathfu/diveq_sca/diveq_sca_depth0_relaxed.rkt
@@ -0,0 +1,120 @@
+#lang rosette
+(require "./bounded.rkt")
+(require "./utils.rkt")
+(require rosette/lib/angelic rosette/lib/match rosette/lib/synthax)
+(require rosette/solver/smt/bitwuzla)
+(current-solver (bitwuzla #:path "/Users/sahilbhatia/Documents/bitwuzla/build/src/main/bitwuzla" #:options (hash ':seed 0)))
+
+
+ (define-bounded (vec_elemwise_add x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (+ (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_add (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_sub x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (- (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_sub (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_mul x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (* (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_mul (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_elemwise_div x y)
+(if (or (< (length x ) 1 ) (! (equal? (length x ) (length y ) ) ) ) (list-empty ) (list-prepend (quotient-noerr (list-ref-noerr x 0 ) (list-ref-noerr y 0 ) ) (vec_elemwise_div (list-tail-noerr x 1 ) (list-tail-noerr y 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_add a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (+ a (list-ref-noerr x 0 ) ) (vec_scalar_add a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_sub a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (- (list-ref-noerr x 0 ) a ) (vec_scalar_sub a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_mul a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (* a (list-ref-noerr x 0 ) ) (vec_scalar_mul a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (vec_scalar_div a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (quotient-noerr (list-ref-noerr x 0 ) a ) (vec_scalar_div a (list-tail-noerr x 1 ) ) ) ))
+
+
+ (define-bounded (scalar_vec_sub a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (- a (list-ref-noerr x 0 ) ) (scalar_vec_sub a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (scalar_vec_div a x)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (quotient-noerr a (list-ref-noerr x 0 ) ) (scalar_vec_div a (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (vec_map x map_int_to_int)
+(if (< (length x ) 1 ) (list-empty ) (list-prepend (map_int_to_int (list-ref-noerr x 0 )) (vec_map (list-tail-noerr x 1 ) map_int_to_int ) ) ))
+
+
+ (define-bounded (reduce_max x)
+(if (<= (length x ) 1 ) (list-ref-noerr x 0 ) (if (> (list-ref-noerr x 0 ) (reduce_max (list-tail-noerr x 1 )) ) (list-ref-noerr x 0 ) (reduce_max (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (reduce_sum x)
+(if (< (length x ) 1 ) 0 (+ (list-ref-noerr x 0 ) (reduce_sum (list-tail-noerr x 1 )) ) ))
+
+
+ (define-bounded (reduce_mul x)
+(if (< (length x ) 1 ) 1 (* (list-ref-noerr x 0 ) (reduce_mul (list-tail-noerr x 1 )) ) ))
+
+(define-grammar (diveq_sca_inv0_gram a agg.result b i n ref.tmp)
+ [rv (choose (&& (&& (>= i (v0) ) (<= i (v1) ) ) (equal? agg.result (v2) ) ))]
+[v0 (choose 0 (- 0 1 ) (+ 0 1 ))]
+[v1 (choose n (- n 1 ) (+ n 1 ))]
+[v2 (choose (vec-slice-noerr a (v3) (v3) ))]
+[v3 (choose (v4))]
+[v4 (choose (v5) (- (v5) 1 ) (+ (v5) 1 ))]
+[v5 (choose 0 n i b)]
+)
+
+(define-grammar (diveq_sca_ps_gram a b n diveq_sca_rv)
+ [rv (choose (equal? diveq_sca_rv (v0) ))]
+[v0 (choose (vec-slice-noerr a (v1) (v1) ))]
+[v1 (choose (v2))]
+[v2 (choose (v3) (- (v3) 1 ) (+ (v3) 1 ))]
+[v3 (choose 0 n b)]
+)
+
+(define-grammar (map_int_to_int_gram int_x)
+ [rv (choose (v0))]
+[v0 (choose (integer-exp-noerr int_x ) (integer-sqrt-noerr int_x ))]
+)
+
+(define (diveq_sca_inv0 a agg.result b i n ref.tmp) (diveq_sca_inv0_gram a agg.result b i n ref.tmp #:depth 10))
+(define (diveq_sca_ps a b n diveq_sca_rv) (diveq_sca_ps_gram a b n diveq_sca_rv #:depth 10))
+
+(define (map_int_to_int int_x) (map_int_to_int_gram int_x #:depth 10))
+
+(define-symbolic a_BOUNDEDSET-len integer?)
+(define-symbolic a_BOUNDEDSET-0 integer?)
+(define-symbolic a_BOUNDEDSET-1 integer?)
+(define a (take (list a_BOUNDEDSET-0 a_BOUNDEDSET-1) a_BOUNDEDSET-len))
+(define-symbolic agg.result_BOUNDEDSET-len integer?)
+(define-symbolic agg.result_BOUNDEDSET-0 integer?)
+(define-symbolic agg.result_BOUNDEDSET-1 integer?)
+(define agg.result (take (list agg.result_BOUNDEDSET-0 agg.result_BOUNDEDSET-1) agg.result_BOUNDEDSET-len))
+(define-symbolic b integer?)
+(define-symbolic diveq_sca_rv_BOUNDEDSET-len integer?)
+(define-symbolic diveq_sca_rv_BOUNDEDSET-0 integer?)
+(define-symbolic diveq_sca_rv_BOUNDEDSET-1 integer?)
+(define diveq_sca_rv (take (list diveq_sca_rv_BOUNDEDSET-0 diveq_sca_rv_BOUNDEDSET-1) diveq_sca_rv_BOUNDEDSET-len))
+(define-symbolic i integer?)
+(define-symbolic n integer?)
+(define-symbolic ref.tmp integer?)
+(current-bitwidth 6)
+(define (assertions)
+ (assert (&& (&& (=> (&& (>= n 1 ) (>= (length a ) n ) ) (diveq_sca_inv0 a (list-empty ) b 0 n 0) ) (=> (&& (&& (&& (< i n ) (>= n 1 ) ) (>= (length a ) n ) ) (diveq_sca_inv0 a agg.result b i n ref.tmp) ) (diveq_sca_inv0 a (list-append agg.result (quotient-noerr (list-ref-noerr a i ) b ) ) b (+ i 1 ) n (quotient-noerr (list-ref-noerr a i ) b )) ) ) (=> (or (&& (&& (&& (! (< i n ) ) (>= n 1 ) ) (>= (length a ) n ) ) (diveq_sca_inv0 a agg.result b i n ref.tmp) ) (&& (&& (&& (&& (! true ) (! (< i n ) ) ) (>= n 1 ) ) (>= (length a ) n ) ) (diveq_sca_inv0 a agg.result b i n ref.tmp) ) ) (diveq_sca_ps a b n agg.result) ) )))
+
+
+    (define sol0
+        (synthesize
+            #:forall (list a_BOUNDEDSET-len a_BOUNDEDSET-0 a_BOUNDEDSET-1 agg.result_BOUNDEDSET-len agg.result_BOUNDEDSET-0 agg.result_BOUNDEDSET-1 b diveq_sca_rv_BOUNDEDSET-len diveq_sca_rv_BOUNDEDSET-0 diveq_sca_rv_BOUNDEDSET-1 i n ref.tmp)
+            #:guarantee (assertions)
+        )
+    )
+    (sat? sol0)
+    (print-forms sol0)
