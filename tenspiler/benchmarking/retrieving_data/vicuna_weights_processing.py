ac076c244be5ea5194f396ad28bc539a963228d3
C <colincai09@gmail.com>
2024-05-13 21:24:27 -0700

    update weight processing script

--- tenspiler/benchmarking/retrieving_data/vicuna_weights_processing.py
+++ tenspiler/benchmarking/retrieving_data/vicuna_weights_processing.py
@@ -1,19 +1,16 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import h5py
+import os
 model33b = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-33b-v1.3")
 
-model33b.save_pretrained("<some_location>", safe_serialization=True, max_shard_size="100GB")
-
 weights33b = model33b.state_dict()
-with h5py.File('./vicuna_weight_full.h5', 'w') as h5f:
+with h5py.File('./vicuna_weight33b_full.h5', 'w') as h5f:
     for k, v in weights33b.items():
         h5f.create_dataset(k, data=v.cpu().numpy())
     print(len(weights33b))
 print("Weights for vicuna 33B are saved")
 
 model7b = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-7b-v1.5")
-
-model7b.save_pretrained("<some_location>", safe_serialization=True, max_shard_size="100GB")
 
 weights7b = model7b.state_dict()
 with h5py.File('./vicuna_weight7b_full.h5', 'w') as h5f:
