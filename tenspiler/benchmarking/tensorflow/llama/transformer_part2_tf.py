c7a55b74c22dc30682d0dcc1b3ef3efc5c87d38e
C <colincai09@gmail.com>
2024-05-13 23:15:48 -0700

    minor change to transformer1/2

--- tenspiler/benchmarking/tensorflow/llama/transformer_part2_tf.py
+++ tenspiler/benchmarking/tensorflow/llama/transformer_part2_tf.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 
 def transformer_part2_tf(token_position, head, head_size, key_cache_layer, attention):
-    return tf.linalg.matvec(tf.transpose(key_cache_layer[:(token_position) + (1)][:, (head) * (head_size):(head) * (head_size) + head_size]), attention[:(token_position) + (1)])
+    return tf.linalg.matvec(tf.transpose(key_cache_layer[0:(token_position) + (1)][:, (head) * (head_size):(head) * (head_size) + head_size]), attention[:(token_position) + (1)])
 
 def transformer_part2_tf_glued(token_position, head, head_size, key_cache_layer, attention):
     key_cache_layer = tf.convert_to_tensor(key_cache_layer, dtype=tf.float32)
