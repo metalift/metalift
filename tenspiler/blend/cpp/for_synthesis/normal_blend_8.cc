132c6cd4c45c98eacb5591fe286db27e99d42829
Jie Qiu <jieqiu0630@gmail.com>
2024-08-24 17:08:25 -0400

    Really fix transformer part2

--- tenspiler/blend/cpp/for_synthesis/normal_blend_8.cc
+++ tenspiler/blend/cpp/for_synthesis/normal_blend_8.cc
@@ -4,8 +4,9 @@ using namespace std;
 vector<int> normal_blend_8(vector<int> base, vector<int> active, int opacity)
 {
   vector<int> out;
-  for (int i = 0; i < base.size(); ++i)
+  for (int i = 0; i < base.size(); ++i) {
     out.push_back(opacity * active[i] + (32 - opacity) * base[i]);
+  }
 
   return out;
 }
