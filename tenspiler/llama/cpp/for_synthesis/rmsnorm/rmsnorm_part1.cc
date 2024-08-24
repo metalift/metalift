a493647ebd1ea2e72f205f96f57bfb7a9c565761
Jie Qiu <jieqiu0630@gmail.com>
2024-08-24 16:50:08 -0400

    Fix transformer part2 draft

--- tenspiler/llama/cpp/for_synthesis/rmsnorm/rmsnorm_part1.cc
+++ tenspiler/llama/cpp/for_synthesis/rmsnorm/rmsnorm_part1.cc
@@ -3,8 +3,8 @@ using namespace std;
 
 int rmsnorm_part1(vector<int> input, vector<int> weight) {
     int ss = 0;
-    for (int i = 0; i < input.size(); i++)
+    for (int i = 0; i < input.size(); i++) {
         ss += input[i] * input[i];
+    }
     return ss;
 }
-
