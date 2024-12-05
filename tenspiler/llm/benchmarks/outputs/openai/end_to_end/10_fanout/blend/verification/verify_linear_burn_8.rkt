cb82da97e417c7c942fe181b0fb2c43233736d6c
Jie Qiu <jieqiu0630@gmail.com>
2024-12-05 00:05:08 -0500

    Remove outputs

--- tenspiler/llm/benchmarks/outputs/openai/end_to_end/10_fanout/blend/verification/verify_linear_burn_8.rkt
+++
@@ -1,6 +0,0 @@
-#lang rosette
-(require "./bounded.rkt")
-(require "./utils.rkt")
-(require rosette/lib/angelic rosette/lib/match rosette/lib/synthax)
-(require rosette/solver/smt/bitwuzla)
-(current-solver (bitwuzla #:path "/Users/jieq/Desktop/bitwuzla/build/src/main/bitwuzla" #:options (hash ':seed 0)))
