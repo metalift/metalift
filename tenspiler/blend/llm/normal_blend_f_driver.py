52989ecd835ef5041440a6f76ccef75d87fda172
Jie Qiu <jieqiu0630@gmail.com>
2024-12-11 22:55:11 +0800

    Clean up and migrate all current llm driver files

--- tenspiler/blend/llm/normal_blend_f_driver.py
+++ tenspiler/blend/llm/normal_blend_f_driver.py
@@ -1,15 +1,11 @@
 import time
 from pathlib import Path
 
-from llm.synthesis import (
-    LLMModel,
-    SingleLoopInfo,
-    get_inv_args,
-    replace_args,
-    run_llm_synthesis_algorithm,
-)
+from llm.synthesis import LLMModel, run_llm_synthesis_algorithm
+from llm.utils import SingleLoopInfo, get_inv_args, replace_args
 from metalift.frontend.llvm import Driver, InvGrammar
 from metalift.ir import Int, List
+from tenspiler.constants import TENSPILER_FN_NAME_TO_AXIOMS, TENSPILER_FNS
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -53,6 +49,8 @@ if __name__ == "__main__":
         source_code=input_code,
         benchmark_name="normal_blend_f",
         llm_model=LLMModel.GPT,
+        dsl_fns=TENSPILER_FNS,
+        dsl_fn_name_to_axioms=TENSPILER_FN_NAME_TO_AXIOMS,
     )
     end_time = time.time()
     print(f"Synthesis took {end_time - start_time} seconds")
