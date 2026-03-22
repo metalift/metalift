ef14dffab059ddd8d6500f086823fe5971d192dc
Jie Qiu <jieqiu0630@gmail.com>
2026-03-22 19:21:26 -0400

    Clean up

--- llm/synthesis.py
+++ llm/synthesis.py
@@ -421,11 +421,7 @@ def run_llm_synthesis_algorithm(
             ]
         else:
             messages_for_new_sol = [inv_template_message]
-        # ps_sol = get_solution_from_llm(llm_model, messages_for_new_sol)
-        ps_sol = """
-def rmsnorm_part2(input: List[int], weight: List[int], ss: int) -> List[int]:
-    return vec_scalar_mul(len(input) // ss, vec_elemwise_mul(input, weight))
-"""
+        ps_sol = get_solution_from_llm(llm_model, messages_for_new_sol)
         ps_sols.append(ps_sol)
 
         # Check if the solution passes the parser. If it does, we can continue to the next step. Otherwise, we would like to generate another PS.
@@ -535,11 +531,7 @@ def rmsnorm_part2(input: List[int], weight: List[int], ss: int) -> List[int]:
                 ]
             else:
                 messages_for_new_sol = [inv_template_message]
-            # inv_sol = get_solution_from_llm(llm_model, messages_for_new_sol)
-            inv_sol = """
-def invariant(output: List[int], i: int, input: List[int], ss: int, weight: List[int]) -> bool:
-    return i >= 0 and i <= len(input) and output == vec_scalar_mul((len(input) // ss), vec_elemwise_mul(input[:i], weight[:i]))
-"""
+            inv_sol = get_solution_from_llm(llm_model, messages_for_new_sol)
             if inv_sol is None:
                 print("Did not generate invariant solution")
                 continue
